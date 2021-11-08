ee2f9fcf701f070c172803e5ceed92b152cc195b
Park DongHa <luncliff@gmail.com>
2021-11-08 09:47:39 +0900

    [tensorflow-lite] Support GPU build in Windows (#22)
    * [tensorflow-lite] support GPU build in Windows
    
    * Requires Vulkan, OpenGL ES, EGL, OpenCL
    * Feature "google-angle" to install "angle" in advance
    
    * [tensorflow-lite] rewrite header remove

--- ports/tensorflow-lite/portfile.cmake
+++ ports/tensorflow-lite/portfile.cmake
@@ -9,16 +9,14 @@ vcpkg_from_github(
     SHA512 d052da4b324f1b5ac9c904ac3cca270cefbf916be6e5968a6835ef3f8ea8c703a0b90be577ac5205edf248e8e6c7ee8817b6a1b383018bb77c381717c6205e05
     PATCHES
         fix-cmakelists.patch
-        fix-header-usage.patch
+        fix-gpu-build-windows.patch
+        fix-eigen3-import.patch
 )
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
     FEATURES
         gpu      TFLITE_ENABLE_GPU
 )
-if("gpu" IN_LIST FEATURES)
-    # run codegen for "tensorflow/lite/delegates/gpu/cl/serialization_generated.h"
-endif()
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}/tensorflow/lite"
@@ -27,6 +25,10 @@ vcpkg_cmake_configure(
         -DTFLITE_ENABLE_XNNPACK=ON
         -DTFLITE_ENABLE_NNAPI=${VCPKG_TARGET_IS_ANDROID}
 )
+if("gpu" IN_LIST FEATURES)
+    # run codegen for ".fbs" files
+    vcpkg_cmake_build(TARGET gl_delegate_codegen)
+endif()
 vcpkg_cmake_install()
 vcpkg_copy_pdbs()
 
@@ -34,20 +36,15 @@ file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include"
                     "${CURRENT_PACKAGES_DIR}/debug/share"
-                    "${CURRENT_PACKAGES_DIR}/include/tensorflow/lite/testdata"
                     "${CURRENT_PACKAGES_DIR}/include/tensorflow/lite/python" # todo: Python feature
-                    "${CURRENT_PACKAGES_DIR}/include/tensorflow/lite/java"
-                    "${CURRENT_PACKAGES_DIR}/include/tensorflow/lite/tools"
-                    "${CURRENT_PACKAGES_DIR}/include/tensorflow/lite/micro"
-                    "${CURRENT_PACKAGES_DIR}/include/tensorflow/lite/delegates" # todo: NNAPI, GPU feature
-                    "${CURRENT_PACKAGES_DIR}/include/tensorflow/lite/lib_package"
-                    "${CURRENT_PACKAGES_DIR}/include/tensorflow/lite/testing"
-                    "${CURRENT_PACKAGES_DIR}/include/tensorflow/lite/examples"
-                    "${CURRENT_PACKAGES_DIR}/include/tensorflow/lite/toco"
-                    "${CURRENT_PACKAGES_DIR}/include/tensorflow/lite/experimental"
-                    "${CURRENT_PACKAGES_DIR}/include/tensorflow/lite/g3doc"
-                    "${CURRENT_PACKAGES_DIR}/include/tensorflow/lite/tutorials"
-                    "${CURRENT_PACKAGES_DIR}/include/tensorflow/lite/ios"
-                    "${CURRENT_PACKAGES_DIR}/include/tensorflow/lite/objc"
-                    "${CURRENT_PACKAGES_DIR}/include/tensorflow/lite/swift"
 )
+if("gpu" IN_LIST FEATURES)
+    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/include/tensorflow/lite/delegates/flex"
+                        "${CURRENT_PACKAGES_DIR}/include/tensorflow/lite/delegates/coreml"
+                        "${CURRENT_PACKAGES_DIR}/include/tensorflow/lite/delegates/java"
+                        "${CURRENT_PACKAGES_DIR}/include/tensorflow/lite/delegates/hexagon"
+                        "${CURRENT_PACKAGES_DIR}/include/tensorflow/lite/delegates/nnapi" # todo: NNAPI if Android
+    )
+else()
+    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/include/tensorflow/lite/delegates")
+endif()
