75e1e26d7fd3f6a5d688cb4df9df9d69959572f7
luncliff <luncliff@gmail.com>
2021-11-13 16:53:43 +0900

    [tensorpipe] update, fix find_package failure

--- ports/tensorpipe/portfile.cmake
+++ ports/tensorpipe/portfile.cmake
@@ -4,20 +4,18 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO pytorch/tensorpipe
-    REF c5a21994bc766659f7f85edb75478e13f429f46c
-    SHA512 05a3989286610edea2f0fcfb9197c563e947f0386f2b52110dd060053d68b62477fcd4778a45a3030b9a53b6ceccead91e4dd96c9f0153023e97414459fbd8dd
+    REF d2aa3485e8229c98891dfd604b514a39d45a5c99
+    SHA512 fbefc18792458ac2234045df8e3cce8dbb17a5e719258f020c2c1d388092358bd2562e53a0377ca18f40bcfbeae4367c277a74c31c5e45296b891453a962e460
     PATCHES
         fix-cmakelists.patch
-        support-test.patch
-        support-pybind11.patch
 )
+file(COPY "${CMAKE_CURRENT_LIST_DIR}/TensorpipeConfig.cmake.in" DESTINATION "${SOURCE_PATH}/cmake")
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
     FEATURES
         cuda        TP_USE_CUDA
         cuda        TP_ENABLE_CUDA_IPC
         pybind11    TP_BUILD_PYTHON
-        test        TP_BUILD_TESTING
 )
 
 if("pybind11" IN_LIST FEATURES)
@@ -26,7 +24,7 @@ if("pybind11" IN_LIST FEATURES)
 endif()
 
 vcpkg_cmake_configure(
-    SOURCE_PATH ${SOURCE_PATH}
+    SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         ${FEATURE_OPTIONS}
         -DTP_ENABLE_SHM=${VCPKG_TARGET_IS_LINUX}
@@ -34,12 +32,15 @@ vcpkg_cmake_configure(
         -DTP_ENABLE_CMA=OFF
         -DTP_BUILD_LIBUV=OFF # will use libuv package
         -DTP_ENABLE_CUDA_GDR=OFF
+        -DTP_BUILD_TESTING=OFF
+    MAYBE_UNUSED_VARIABLES
+        TP_ENABLE_CUDA_GDR
+        TP_ENABLE_CUDA_IPC
 )
 vcpkg_cmake_install()
-vcpkg_copy_pdbs()
-vcpkg_cmake_config_fixup()
+vcpkg_cmake_config_fixup(CONFIG_PATH "share/cmake/Tensorpipe")
 
-file(INSTALL ${SOURCE_PATH}/LICENSE.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include
-                    ${CURRENT_PACKAGES_DIR}/debug/share
+file(INSTALL "${SOURCE_PATH}/LICENSE.txt" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include"
+                    "${CURRENT_PACKAGES_DIR}/debug/share"
 )
