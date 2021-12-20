34ed8251500ec666510b4ac25e8ff7ad18d041f2
Park DongHa <luncliff@gmail.com>
2021-12-20 16:33:55 +0900

    [mediapipe] include calculator sources

--- ports/mediapipe/portfile.cmake
+++ ports/mediapipe/portfile.cmake
@@ -8,8 +8,8 @@ vcpkg_from_github(
     REF v0.8.8
     SHA512 76c6e1ccb56a1fa403376ad32805f8cffb09dba9f99e9f0797e3af43937ade6955833ed660c723264618c7e7bd2c3dbe8b9d0ca475cf079920627b47ee4e6752
     HEAD_REF master
-    PATCHES
-        fix-sources.patch
+    # PATCHES
+    #     fix-sources.patch
 )
 file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
 
