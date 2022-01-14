04f2d7ed342c9bfae2fd8a2a649b9ea2d8bcf09f
Park DongHa <luncliff@gmail.com>
2022-01-14 20:52:20 +0900

    scripts: new script to `x-add-version`
    * fix mistype of registry-format.ps1

--- scripts/registry-add-version.ps1
+++ scripts/registry-add-version.ps1
@@ -2,17 +2,18 @@ using namespace System
 using namespace System.IO
 param
 (
+    [String]$Port,
     [String]$VcpkgRoot = $env:VCPKG_ROOT,
     [String]$RegistryRoot = (Join-Path -Path $env:VCPKG_ROOT "vcpkg-registry"),
     [String]$Workspace = (Get-Location)
 )
 
-function RunFormat($root) {
+function AddVersion([String]$root, [String]$port) {
     vcpkg.exe version
-    vcpkg.exe format-manifest --all `
+    vcpkg.exe x-add-version $port `
         --x-builtin-ports-root="$root/ports" `
         --x-builtin-registry-versions-dir="$root/versions"
 }
 
 $env:Path = "$VcpkgRoot;$env:Path"
-RunFormat $RegistryRoot
+AddVersion $RegistryRoot $Port
