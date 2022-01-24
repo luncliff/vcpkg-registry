f8f1ee4ceac100303b42aad534d316755ad6bca0
Park DongHa <luncliff@gmail.com>
2022-01-24 19:31:13 +0900

    [directml] Update portfile.cmake
    * require "dynamic" CRT
    * fix install path for header files

--- ports/directml/portfile.cmake
+++ ports/directml/portfile.cmake
@@ -1,5 +1,4 @@
-
-vcpkg_check_linkage(ONLY_DYNAMIC_LIBRARY) # [ONLY_STATIC_CRT | ONLY_DYNAMIC_CRT]
+vcpkg_check_linkage(ONLY_DYNAMIC_LIBRARY ONLY_DYNAMIC_CRT)
 vcpkg_find_acquire_program(NUGET)
 
 set(PACKAGE_NAME    "Microsoft.AI.DirectML")
@@ -15,34 +14,34 @@ vcpkg_execute_required_process(
 
 get_filename_component(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/${PACKAGE_NAME}.${PACKAGE_VERSION} ABSOLUTE)
 if(TARGET_TRIPLET STREQUAL x64-windows)
-    set(BIN_FOLDER "x64-win")
+    set(TRIPLE "x64-win")
 elseif(TARGET_TRIPLET STREQUAL x86-windows)
-    set(BIN_FOLDER "x86-win")
+    set(TRIPLE "x86-win")
 elseif(TARGET_TRIPLET STREQUAL arm64-windows)
-    set(BIN_FOLDER "arm64-win")
+    set(TRIPLE "arm64-win")
 elseif(TARGET_TRIPLET STREQUAL arm-windows)
-    set(BIN_FOLDER "arm-win")
+    set(TRIPLE "arm-win")
 elseif(TARGET_TRIPLET STREQUAL x64-linux)
-    set(BIN_FOLDER "x64-linux")
+    set(TRIPLE "x64-linux")
 else()
     message(FATAL_ERROR "The triplet '${TARGET_TRIPLET}' is not supported")
 endif()
 
 if(VCPKG_TARGET_IS_WINDOWS)
-    file(INSTALL ${SOURCE_PATH}/${BIN_FOLDER}/DirectML.dll       DESTINATION ${CURRENT_PACKAGES_DIR}/bin)
-    file(INSTALL ${SOURCE_PATH}/${BIN_FOLDER}/DirectML.pdb       DESTINATION ${CURRENT_PACKAGES_DIR}/bin)
-    file(INSTALL ${SOURCE_PATH}/${BIN_FOLDER}/DirectML.lib       DESTINATION ${CURRENT_PACKAGES_DIR}/lib)
-    file(INSTALL ${SOURCE_PATH}/${BIN_FOLDER}/DirectML.Debug.dll DESTINATION ${CURRENT_PACKAGES_DIR}/debug/bin)
-    file(INSTALL ${SOURCE_PATH}/${BIN_FOLDER}/DirectML.Debug.pdb DESTINATION ${CURRENT_PACKAGES_DIR}/debug/bin)
-    file(INSTALL ${SOURCE_PATH}/${BIN_FOLDER}/DirectML.lib       DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib)
+    file(INSTALL ${SOURCE_PATH}/bin/${TRIPLE}/DirectML.dll       DESTINATION ${CURRENT_PACKAGES_DIR}/bin)
+    file(INSTALL ${SOURCE_PATH}/bin/${TRIPLE}/DirectML.pdb       DESTINATION ${CURRENT_PACKAGES_DIR}/bin)
+    file(INSTALL ${SOURCE_PATH}/bin/${TRIPLE}/DirectML.lib       DESTINATION ${CURRENT_PACKAGES_DIR}/lib)
+    file(INSTALL ${SOURCE_PATH}/bin/${TRIPLE}/DirectML.Debug.dll DESTINATION ${CURRENT_PACKAGES_DIR}/debug/bin)
+    file(INSTALL ${SOURCE_PATH}/bin/${TRIPLE}/DirectML.Debug.pdb DESTINATION ${CURRENT_PACKAGES_DIR}/debug/bin)
+    file(INSTALL ${SOURCE_PATH}/bin/${TRIPLE}/DirectML.lib       DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib)
 elseif(VCPKG_TARGET_IS_LINUX)
-    file(INSTALL ${SOURCE_PATH}/${BIN_FOLDER}/libdirectml.so     DESTINATION ${CURRENT_PACKAGES_DIR}/lib)
-    file(INSTALL ${SOURCE_PATH}/${BIN_FOLDER}/libdirectml.so     DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib)
+    file(INSTALL ${SOURCE_PATH}/bin/${TRIPLE}/libdirectml.so     DESTINATION ${CURRENT_PACKAGES_DIR}/lib)
+    file(INSTALL ${SOURCE_PATH}/bin/${TRIPLE}/libdirectml.so     DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib)
 else()
     message(FATAL_ERROR "The target platform is not supported")
 endif()
 
-file(INSTALL ${SOURCE_PATH}/include DESTINATION ${CURRENT_PACKAGES_DIR}/include)
+file(INSTALL ${SOURCE_PATH}/include DESTINATION ${CURRENT_PACKAGES_DIR})
 
 file(INSTALL ${SOURCE_PATH}/LICENSE.txt
      DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright
