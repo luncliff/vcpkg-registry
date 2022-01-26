f0a5e6b5a3a95d3a706a425f2164ee4316dc7dd9
Park DongHa <luncliff@gmail.com>
2022-01-27 04:22:36 +0900

    [tensorflow-lite] update to v2.6.1 (#32)
    * [tensorflow-lite] update to 2.6.1
    
    * rewrite some patch files
    
    * [fft2d] create a new port
    
    * ci: build job for 'fft2d'
    
    * update version JSON
    
    * [tensorflow-lite] use 'fft2d' package
    
    * [tensorflow-lite] update version JSON
    
    * [tensorflow-lite] fixing gpu feature failure in x64-osx
    
    * [tensorflow-lite] build with Obj-C++ and CoreML proto
    
    * [tensorflow-lite] split gpu source/cmake patch
    
    * reduced patch size
    * update version JSON
    
    * [tensorflow-lite] inject tool path
    
    * Update azure-pipelines.yml
    
    * report build logs for tensorflow-lite
    * always install EGL/OpenGL registry

--- ports/tensorflow-lite/portfile.cmake
+++ ports/tensorflow-lite/portfile.cmake
@@ -5,13 +5,32 @@ vcpkg_check_linkage(ONLY_DYNAMIC_LIBRARY)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO tensorflow/tensorflow
-    REF v2.6.0
-    SHA512 d052da4b324f1b5ac9c904ac3cca270cefbf916be6e5968a6835ef3f8ea8c703a0b90be577ac5205edf248e8e6c7ee8817b6a1b383018bb77c381717c6205e05
+    REF v2.6.1
+    SHA512 34dcec08b73ef25cb6e5bcb0e083c2f43d8364bc9a465e59d63dc3f162a129d011e03faaecf7d563cbbe39f0c2bbf2d1795ccfb8b2ea3f108ed8db992cea9475
     PATCHES
         fix-cmakelists.patch
-        fix-gpu-build-windows.patch
         fix-eigen3-import.patch
+        fix-gpu-build.patch
+        fix-gpu-sources.patch
 )
+if(VCPKG_TARGET_IS_OSX AND ("gpu" IN_LIST FEATURES))
+    # .proto files for coreml_mlmodel_codegen
+    vcpkg_from_github(
+        OUT_SOURCE_PATH COREML_SOURCE_PATH
+        REPO apple/coremltools
+        REF 5.1
+        SHA512 4cabfcdee33e2f45626a80e2a9a2aa94ffe33bfc6daff941fca32b1b8428eeec6f65f552b9370c38fa6fca7aa075ff303e1c9b9cdf6cb680b41b6185e94ce36f
+        HEAD_REF main
+    )
+    find_program(FLATC_EXECUTABLE NAMES flatc PATHS ${CURRENT_HOST_INSTALLED_DIR}/tools/flatbuffers REQUIRED NO_DEFAULT_PATH NO_CMAKE_PATH)
+    message(STATUS "Using flatc: ${FLATC_EXECUTABLE}")
+    find_program(PROTOC_EXECUTABLE NAMES protoc PATHS ${CURRENT_HOST_INSTALLED_DIR}/tools/protobuf REQUIRED NO_DEFAULT_PATH NO_CMAKE_PATH)
+    message(STATUS "Using protoc: ${PROTOC_EXECUTABLE}")
+    list(APPEND PLATFORM_OPTIONS
+        -DFLATC_EXECUTABLE:FILE_PATH=${FLATC_EXECUTABLE}
+        -DPROTOC_EXECUTABLE:FILE_PATH=${PROTOC_EXECUTABLE}
+    )
+endif()
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
     FEATURES
@@ -21,9 +40,11 @@ vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}/tensorflow/lite"
     OPTIONS
-        ${FEATURE_OPTIONS}
+        ${FEATURE_OPTIONS} ${PLATFORM_OPTIONS}
+        -DTFLITE_ENABLE_RUY=ON
         -DTFLITE_ENABLE_XNNPACK=ON
         -DTFLITE_ENABLE_NNAPI=${VCPKG_TARGET_IS_ANDROID}
+        -DCOREML_SOURCE_DIR=${COREML_SOURCE_PATH}
 )
 if("gpu" IN_LIST FEATURES)
     # run codegen for ".fbs" files
@@ -40,11 +61,17 @@ file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include"
 )
 if("gpu" IN_LIST FEATURES)
     file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/include/tensorflow/lite/delegates/flex"
-                        "${CURRENT_PACKAGES_DIR}/include/tensorflow/lite/delegates/coreml"
                         "${CURRENT_PACKAGES_DIR}/include/tensorflow/lite/delegates/java"
                         "${CURRENT_PACKAGES_DIR}/include/tensorflow/lite/delegates/hexagon"
-                        "${CURRENT_PACKAGES_DIR}/include/tensorflow/lite/delegates/nnapi" # todo: NNAPI if Android
     )
 else()
-    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/include/tensorflow/lite/delegates")
+    # remove unsupported delegated for each target platform
+    if(NOT DEFINED VCPKG_TARGET_IS_OSX OR NOT VCPKG_TARGET_IS_OSX)
+        file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/include/tensorflow/lite/delegates/coreml")
+    endif()
+    if(NOT DEFINED VCPKG_TARGET_IS_ANDROID OR NOT VCPKG_TARGET_IS_ANDROID)
+        file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/include/tensorflow/lite/nnapi"
+                            "${CURRENT_PACKAGES_DIR}/include/tensorflow/lite/delegates/nnapi"
+        )
+    endif()
 endif()
