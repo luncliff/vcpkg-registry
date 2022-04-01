1b9219a565646cfe3be4f7749edea353c0264b86
luncliff <luncliff@gmail.com>
2022-04-02 01:58:44 +0900

    [gemmlowp] use FindThreads cmake module

---
+++ ports/gemmlowp/portfile.cmake
@@ -0,0 +1,24 @@
+if(NOT VCPKG_TARGET_IS_WINDOWS)
+    vcpkg_check_linkage(ONLY_DYNAMIC_LIBRARY)
+endif()
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO google/gemmlowp
+    REF e844ffd17118c1e17d94e1ba4354c075a4577b88
+    SHA512 934bc79cc951dd18bff413148f3526332685bf0980ddcc0f990ba347bafcd73725e5ffd4bd255dcaa21f0fd71698d26726e2f9f36aa8df87380048041e536e20
+    HEAD_REF master
+    PATCHES
+        fix-cmakelists.patch
+)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}/contrib"
+    OPTIONS
+        -DBUILD_TESTING=OFF
+)
+vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/${PORT}")
+
+file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
