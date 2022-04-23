82113b6d49183180b18c9d96f5fd170a16db843f
Park DongHa <luncliff@gmail.com>
2022-04-24 05:00:24 +0900

    [xnnpack] update to latest (2022-04-23) (#43)
    * [xnnpack] update to latest (2022-04-08)
    
    * [xnnpack] update version JSON
    
    * [xnnpack] create a patch for xnnpack-config
    
    * [xnnpack] fix wrong patch file
    
    * Some link configurations were dropped
    
    * [tensorflow-lite] update xnnpack usage
    
    * [tensorflow-lite] update port version for v2.7.1
    
    * [xnnpack] fix arm-android build

--- ports/xnnpack/portfile.cmake
+++ ports/xnnpack/portfile.cmake
@@ -2,16 +2,23 @@ if(VCPKG_TARGET_IS_WINDOWS)
     vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 endif()
 
+if(VCPKG_TARGET_IS_ANDROID AND VCPKG_TARGET_ARCHITECTURE STREQUAL "arm")
+    list(APPEND PATCHES fix-arm-android.patch)
+endif()
+
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO google/XNNPACK
-    REF 1851410d1bdd0189f4c1295d8893dcc9c2504bc8 # 2021-10-10
-    SHA512 6af2227fc4851f7247c2fb6fa508816b362cbd6c8c02c89320a8bb759428666d0d17ece3d9fdca0ac26f0b25461d37b8b8c883a93174b4e7a84e49c4266c48e7
+    REF f334417944c6c8e17c209816cdf8f58228ae4263 # 2022-04-23
+    SHA512 1340f88e6106ac5dc87f4327cbf796ed5124446ad2e58cecb509d19a1a34db25c9de969caec9044840a370bd87fc655b7e231de0538e4de264b83a26c4afa685
     HEAD_REF master
     PATCHES
-        use-packages.patch
         change-allowed-systems.patch
+        use-packages.patch
+        support-package.patch
+        ${PATCHES}
 )
+file(COPY "${CMAKE_CURRENT_LIST_DIR}/xnnpack-config.cmake.in" DESTINATION "${SOURCE_PATH}/cmake")
 
 if(VCPKG_TARGET_IS_IOS)
     if(VCPKG_TARGET_ARCHITECTURE STREQUAL "arm")
@@ -31,12 +38,14 @@ vcpkg_cmake_configure(
         -DXNNPACK_ENABLE_SPARSE=ON
         -DXNNPACK_BUILD_TESTS=OFF
         -DXNNPACK_BUILD_BENCHMARKS=OFF
-       ${PLATFORM_OPTIONS}
+        ${PLATFORM_OPTIONS}
 )
 vcpkg_cmake_install()
-vcpkg_cmake_config_fixup()
+vcpkg_cmake_config_fixup(CONFIG_PATH share)
 
-file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
+file(INSTALL ${SOURCE_PATH}/LICENSE
+     DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright
+)
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include
                     ${CURRENT_PACKAGES_DIR}/debug/bin
                     ${CURRENT_PACKAGES_DIR}/debug/share
