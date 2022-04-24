4b16f77dd35c822abcae1f1ca77c5ee43fd77510
luncliff <luncliff@gmail.com>
2022-04-24 14:50:16 +0900

    [tensorflow-lite] update to v2.8.0
    * Exclude iOS build in "supports"
    * update version JSON

--- ports/tensorflow-lite/portfile.cmake
+++ ports/tensorflow-lite/portfile.cmake
@@ -1,29 +1,20 @@
-
-# Currently links with an internal library 'fft2d'
 vcpkg_check_linkage(ONLY_DYNAMIC_LIBRARY)
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO tensorflow/tensorflow
-    REF v2.7.1
-    SHA512 d818f4c2640dd8240cf66c4c64d79fa90508b5c4af3c2e2905b0535fb0fe9c70d3495d8d1e74f7e92c6c6e986ad70855e31147516811a4800a412a927cb398ea
+    REF v2.8.0
+    SHA512 9cddb78c0392b7810e71917c3731f895e31c250822031ac7f498bf20435408c640b2fba4de439fa4a47c70dbff38b86e50fed2971df1f1916f23f9490241cfed
     PATCHES
-        fix-cmakelists.patch
-        fix-gpu-build.patch
-        fix-gpu-source.patch
+        fix-cmake.patch
+        fix-cmake-gpu.patch
+        fix-cmake-android.patch
+        fix-source-gpu.patch
 )
 file(REMOVE_RECURSE "${SOURCE_PATH}/third_party/eigen3")
 file(CREATE_LINK "${CURRENT_INSTALLED_DIR}/include/eigen3" "${SOURCE_PATH}/third_party/eigen3" SYMBOLIC)
 
-if(VCPKG_TARGET_IS_OSX AND ("gpu" IN_LIST FEATURES))
-    # .proto files for coreml_mlmodel_codegen
-    vcpkg_from_github(
-        OUT_SOURCE_PATH COREML_SOURCE_PATH
-        REPO apple/coremltools
-        REF 5.1
-        SHA512 4cabfcdee33e2f45626a80e2a9a2aa94ffe33bfc6daff941fca32b1b8428eeec6f65f552b9370c38fa6fca7aa075ff303e1c9b9cdf6cb680b41b6185e94ce36f
-        HEAD_REF main
-    )
+if("gpu" IN_LIST FEATURES)
     find_program(FLATC_EXECUTABLE NAMES flatc PATHS ${CURRENT_HOST_INSTALLED_DIR}/tools/flatbuffers REQUIRED NO_DEFAULT_PATH NO_CMAKE_PATH)
     message(STATUS "Using flatc: ${FLATC_EXECUTABLE}")
     find_program(PROTOC_EXECUTABLE NAMES protoc PATHS ${CURRENT_HOST_INSTALLED_DIR}/tools/protobuf REQUIRED NO_DEFAULT_PATH NO_CMAKE_PATH)
@@ -32,6 +23,16 @@ if(VCPKG_TARGET_IS_OSX AND ("gpu" IN_LIST FEATURES))
         -DFLATC_EXECUTABLE:FILE_PATH=${FLATC_EXECUTABLE}
         -DPROTOC_EXECUTABLE:FILE_PATH=${PROTOC_EXECUTABLE}
     )
+    if(VCPKG_TARGET_IS_OSX) # .proto files for coreml_mlmodel_codegen
+        vcpkg_from_github(
+            OUT_SOURCE_PATH COREML_SOURCE_PATH
+            REPO apple/coremltools
+            REF 5.1
+            SHA512 4cabfcdee33e2f45626a80e2a9a2aa94ffe33bfc6daff941fca32b1b8428eeec6f65f552b9370c38fa6fca7aa075ff303e1c9b9cdf6cb680b41b6185e94ce36f
+            HEAD_REF main
+        )
+        list(APPEND PLATFORM_OPTIONS -DCOREML_SOURCE_DIR=${COREML_SOURCE_PATH})
+    endif()
 endif()
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
@@ -43,10 +44,12 @@ vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}/tensorflow/lite"
     OPTIONS
         ${FEATURE_OPTIONS} ${PLATFORM_OPTIONS}
+        -DTFLITE_ENABLE_RESOURCE=ON
         -DTFLITE_ENABLE_RUY=ON
         -DTFLITE_ENABLE_XNNPACK=ON
         -DTFLITE_ENABLE_NNAPI=${VCPKG_TARGET_IS_ANDROID}
-        -DCOREML_SOURCE_DIR=${COREML_SOURCE_PATH}
+    OPTIONS_DEBUG
+        -DTFLITE_ENABLE_NNAPI_VERBOSE_VALIDATION=${VCPKG_TARGET_IS_ANDROID}
     MAYBE_UNUSED_VARIABLES
         COREML_SOURCE_DIR
         FLATC_EXECUTABLE
