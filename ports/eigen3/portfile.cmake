e1c1c2af19b27111704024a04e313bdb05de2753
Park DongHa <luncliff@gmail.com>
2022-06-26 02:24:05 +0900

    CI: Fix Android failure (#50)
    * ci: upgrade image to ubuntu-22.04
    
    * ci: temporary disable windows/osx jobs
    
    * ci: provide ANDROID_NDK_HOME
    
    * run debug mode to detect compiler
    
    * [eigen3] sync port with microsoft/vcpkg
    
    * disable fortran in android
    
    * Revert "ci: temporary disable windows/osx jobs"
    
    This reverts commit 6e45c10bdf0d01b0ffe80c61d2d90bc70264ac68.
    
    * ci: remove log report

--- ports/eigen3/portfile.cmake
+++ ports/eigen3/portfile.cmake
@@ -6,13 +6,22 @@ vcpkg_from_gitlab(
     REPO libeigen/eigen
     REF 3.4.0
     SHA512 ba75ecb760e32acf4ceaf27115468e65d4f77c44f8d519b5a13e7940af2c03a304ad433368cb6d55431f307c5c39e2666ab41d34442db3cf441638e51f5c3b6a
+    HEAD_REF master
+    PATCHES
+        remove_configure_checks.patch # This removes unnecessary configure checks. Eigen3 just installs headers not anything more.
+        fix-vectorized-reductions-half.patch # Remove this patch in the next update
 )
 
+if(VCPKG_TARGET_IS_ANDROID)
+    list(APPEND PLATFORM_OPTIONS -DCMAKE_Fortran_COMPILER=OFF)
+endif()
+
 vcpkg_cmake_configure(
-    SOURCE_PATH ${SOURCE_PATH}
+    SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         -DBUILD_TESTING=OFF
         -DEIGEN_BUILD_PKGCONFIG=ON
+        ${PLATFORM_OPTIONS}
     OPTIONS_RELEASE
         -DCMAKEPACKAGE_INSTALL_DIR=${CURRENT_PACKAGES_DIR}/share/eigen3
         -DPKGCONFIG_INSTALL_DIR=${CURRENT_PACKAGES_DIR}/lib/pkgconfig
@@ -20,10 +29,14 @@ vcpkg_cmake_configure(
         -DCMAKEPACKAGE_INSTALL_DIR=${CURRENT_PACKAGES_DIR}/debug/share/eigen3
         -DPKGCONFIG_INSTALL_DIR=${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig
 )
+
 vcpkg_cmake_install()
-vcpkg_cmake_config_fixup(CONFIG_PATH share/${PORT})
+vcpkg_cmake_config_fixup()
 vcpkg_fixup_pkgconfig()
 
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include ${CURRENT_PACKAGES_DIR}/debug/share)
+file(GLOB INCLUDES "${CURRENT_PACKAGES_DIR}/include/eigen3/*")
+# Copy the eigen header files to conventional location for user-wide MSBuild integration
+file(COPY ${INCLUDES} DESTINATION "${CURRENT_PACKAGES_DIR}/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include" "${CURRENT_PACKAGES_DIR}/debug/share")
 
-file(INSTALL ${SOURCE_PATH}/COPYING.README DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
+file(INSTALL "${SOURCE_PATH}/COPYING.README" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
