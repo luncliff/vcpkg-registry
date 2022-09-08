f595eb4ef00b8ca30abbd7168a2d6d256de95817
luncliff <luncliff@gmail.com>
2022-09-08 18:19:40 +0900

    [cpuinfo] update to latest

--- ports/cpuinfo/portfile.cmake
+++ ports/cpuinfo/portfile.cmake
@@ -6,8 +6,8 @@ endif()
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO pytorch/cpuinfo
-    REF b40bae27785787b6dd70788986fd96434cf90ae2
-    SHA512 dbbe4f3e1d5ae74ffc8ba2cba0ab745a23f4993788f4947825ef5125dd1cbed3e13e0c98e020e6fcfa9879f54f06d7cba4de73ec29f77649b6a27b4ab82c8f1c
+    REF 8ec7bd91ad0470e61cf38f618cc1f270dede599c
+    SHA512 b3342ce0a1f842084ff53efdfd15c44586ac7cd36249211e2925d84aa1f33ee8d6f76cd62ea20e91d8b908c3c8afda5a47516008b69749504024b9813a623ee2
     HEAD_REF master
 )
 
@@ -16,23 +16,35 @@ vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
         tools CPUINFO_BUILD_TOOLS
 )
 
-if(VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
-    list(APPEND LINK_OPTIONS -DCPUINFO_LIBRARY_TYPE=shared)
-else()
-    list(APPEND LINK_OPTIONS -DCPUINFO_LIBRARY_TYPE=static)
-endif()
-
-if(VCPKG_CRT_LINKAGE STREQUAL "dynamic")
-    list(APPEND LINK_OPTIONS -DCPUINFO_RUNTIME_TYPE=shared)
-else()
-    list(APPEND LINK_OPTIONS -DCPUINFO_RUNTIME_TYPE=static)
+if(VCPKG_TARGET_IS_WINDOWS)
+    # It checks CMAKE_SYSTEM_NAME ...
+    if(DEFINED VCPKG_CMAKE_SYSTEM_NAME) # ex) WindowsStore
+        list(APPEND PLATFORM_OPTIONS -DCMAKE_SYSTEM_NAME=${VCPKG_CMAKE_SYSTEM_NAME})
+    else()
+        list(APPEND PLATFORM_OPTIONS -DCMAKE_SYSTEM_NAME=Windows)
+    endif()
+    # And CPUINFO_TARGET_PROCESSOR ...
+    if(VCPKG_TARGET_ARCHITECTURE STREQUAL "x64")
+        list(APPEND PLATFORM_OPTIONS -DCPUINFO_TARGET_PROCESSOR="x64")
+    elseif(VCPKG_TARGET_ARCHITECTURE STREQUAL "x86")
+        list(APPEND PLATFORM_OPTIONS -DCPUINFO_TARGET_PROCESSOR="Win32")
+    elseif(VCPKG_TARGET_ARCHITECTURE STREQUAL "arm64")
+        list(APPEND PLATFORM_OPTIONS -DCPUINFO_TARGET_PROCESSOR="ARM64")
+    else()
+        # Let the project detect the architecture
+        message(WARNING "Unexpected architecture: ${VCPKG_TARGET_ARCHITECTURE}")
+        list(APPEND PLATFORM_OPTIONS -DCPUINFO_TARGET_PROCESSOR=${VCPKG_TARGET_ARCHITECTURE})
+    endif()
 endif()
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
+    WINDOWS_USE_MSBUILD
     OPTIONS
         ${FEATURE_OPTIONS}
-        ${LINK_OPTIONS}
+        ${PLATFORM_OPTIONS}
+        -DCPUINFO_LIBRARY_TYPE:STRING=default
+        -DCPUINFO_RUNTIME_TYPE:STRING=default
         -DCPUINFO_BUILD_UNIT_TESTS=OFF
         -DCPUINFO_BUILD_MOCK_TESTS=OFF
         -DCPUINFO_BUILD_BENCHMARKS=OFF
@@ -46,13 +58,10 @@ vcpkg_cmake_config_fixup()
 vcpkg_copy_pdbs()
 vcpkg_fixup_pkgconfig() # pkg_check_modules(libcpuinfo)
 
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
 if("tools" IN_LIST FEATURES)
-    vcpkg_copy_tools(
-        TOOL_NAMES cache-info cpuid-dump cpu-info isa-info
-        AUTO_CLEAN
-    )
+    vcpkg_copy_tools(TOOL_NAMES cache-info cpuid-dump cpu-info isa-info AUTO_CLEAN)
 endif()
 
 file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
