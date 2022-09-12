03ad942e70518db6cd702fb3c8714e67d76d72fe
luncliff <luncliff@gmail.com>
2022-09-13 02:01:46 +0900

    [xnnpack] basic support for ARM windows

--- ports/xnnpack/portfile.cmake
+++ ports/xnnpack/portfile.cmake
@@ -10,10 +10,23 @@ vcpkg_from_github(
     HEAD_REF master
     PATCHES
         fix-cmake.patch
+        windows-parallel-build.patch
 )
 file(COPY "${CMAKE_CURRENT_LIST_DIR}/xnnpack-config.cmake.in" DESTINATION "${SOURCE_PATH}/cmake")
 
 if(VCPKG_TARGET_IS_WINDOWS)
+    # Visual Studio generator for CMake to detect ASM compiler
+    list(APPEND GENERATOR_OPTIONS WINDOWS_USE_MSBUILD)
+
+    # note: `Assember` class member functions' name collides with MSVC intrinsics...
+    if(VCPKG_TARGET_ARCHITECTURE MATCHES "arm")
+        list(APPEND PLATFORM_OPTIONS
+            -DXNNPACK_ENABLE_ARM_FP16=OFF # `__fp16` type is missing
+            -DXNNPACK_ENABLE_ARM_BF16=OFF # `bfloat16_t` type is missing
+            -DXNNPACK_ENABLE_ARM_DOTPROD=OFF
+        )
+    endif()
+    # CMAKE_SYSTEM_PROCESSOR for the generator
     if(VCPKG_TARGET_ARCHITECTURE STREQUAL "x64")
         list(APPEND PLATFORM_OPTIONS -DCMAKE_SYSTEM_PROCESSOR=x86_64)
     elseif(VCPKG_TARGET_ARCHITECTURE STREQUAL "x86")
@@ -37,16 +50,23 @@ elseif(VCPKG_TARGET_IS_IOS)
     list(APPEND PLATFORM_OPTIONS -DIOS_ARCH=${IOS_ARCH})
 endif()
 
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    FEATURES
+        jit      XNNPACK_ENABLE_JIT
+)
+
 vcpkg_cmake_configure(
     SOURCE_PATH ${SOURCE_PATH}
+    ${GENERATOR_OPTIONS}
     OPTIONS
+        ${FEATURE_OPTIONS}
+        ${PLATFORM_OPTIONS}
         -DXNNPACK_USE_SYSTEM_LIBS=ON
         -DXNNPACK_ENABLE_ASSEMBLY=ON
         -DXNNPACK_ENABLE_MEMOPT=ON
         -DXNNPACK_ENABLE_SPARSE=ON
         -DXNNPACK_BUILD_TESTS=OFF
         -DXNNPACK_BUILD_BENCHMARKS=OFF
-        ${PLATFORM_OPTIONS}
 )
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(CONFIG_PATH share)
