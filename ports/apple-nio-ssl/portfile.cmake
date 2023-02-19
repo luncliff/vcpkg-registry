c3b8ffe0484bf9090c5a757efff0f44010a8d6ec
Park DongHa <luncliff@gmail.com>
2023-02-19 09:16:09 +0900

    [apple-nio-ssl] Fix static/dynamic build with xcodebuild (#83)
    * [apple-nio-ssl] support static, ios simulator build
    
    * [apple-nio-ssl] record target list
    
    * [apple-nio-ssl] use CMake functions
    
    * [apple-nio-ssl] add some comments

--- ports/apple-nio-ssl/portfile.cmake
+++ ports/apple-nio-ssl/portfile.cmake
@@ -1,7 +1,13 @@
-if(VCPKG_TARGET_IS_OSX)
-    vcpkg_check_linkage(ONLY_DYNAMIC_LIBRARY)
-elseif(VCPKG_TARGET_IS_IOS)
-    vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+
+# see https://developer.apple.com/library/archive/documentation/DeveloperTools/Reference/XcodeBuildSettingRef/1-Build_Setting_Reference/build_setting_ref.html
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
+    # By default, the project generates dynamic framework.
+    # These patches have NO effect. But may help debugging in the SOURCE_PATH ...
+    list(APPEND SWIFTPM_PATCHES swiftpm-product-dynamic.patch)
+    # list(APPEND XCODEBUILD_PARAMS "MACH_O_TYPE=mh_dylib")
+elseif(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+    list(APPEND SWIFTPM_PATCHES swiftpm-product-static.patch)
+    list(APPEND XCODEBUILD_PARAMS "MACH_O_TYPE=staticlib")
 endif()
 
 vcpkg_from_github(
@@ -10,6 +16,9 @@ vcpkg_from_github(
     REF 2.23.0
     SHA512 a7d4478f3ebd8dd1ee78c71afef610c33b74fb1b38054ef352dccbc454b8c56b30158c63d20d8468fa17e3f688445814ea743b841bbac28b8639bc81cef86632
     HEAD_REF main
+    PATCHES
+        swiftpm-use-local.patch
+        ${SWIFTPM_PATCHES}
 )
 
 vcpkg_from_github(
@@ -20,29 +29,101 @@ vcpkg_from_github(
     HEAD_REF main
 )
 
-# use source folder so `--editable` can take effect always
-get_filename_component(CUSTOM_BUILDTREES_DIR "${SOURCE_PATH}" PATH)
-file(REMOVE_RECURSE
-    "${CUSTOM_BUILDTREES_DIR}/.build"
-    "${CUSTOM_BUILDTREES_DIR}/build"
-)
+# see vcpkg_extract_source_archive
+if(NOT _VCPKG_EDITABLE)
+    file(REMOVE_RECURSE "${SOURCE_PATH}/.build" "${SOURCE_PATH}/build")
+endif()
 
 # use symbolic link to prevent SwiftPM checkouts
-set(ENV{SWIFTCI_USE_LOCAL_DEPS} "TRUE")
+get_filename_component(CUSTOM_BUILDTREES_DIR "${SOURCE_PATH}" PATH)
 file(CREATE_LINK "${NIO_SOURCE_PATH}" "${CUSTOM_BUILDTREES_DIR}/swift-nio" SYMBOLIC)
 
-# find required tools to build
-find_program(SWIFT NAMES swift REQUIRED)
-message(STATUS "Detected swift: ${SWIFT}")
-find_program(XCODEBUILD NAMES xcodebuild REQUIRED)
-message(STATUS "Detected xcodebuild: ${XCODEBUILD}")
+function(swiftpm_generatate_xcodeproj)
+    cmake_parse_arguments(PARSE_ARGV 0 swiftpm "" "LOGNAME" "PARAMS")
+    if(DEFINED xc_UNPARSED_ARGUMENTS)
+        message(FATAL_ERROR "${CMAKE_CURRENT_FUNCTION} can't handle extra arguments: ${xc_UNPARSED_ARGUMENTS}")
+    endif()
+    if(NOT DEFINED swiftpm_LOGNAME)
+        set(swiftpm_LOGNAME "generate")
+    endif()
+    if(NOT DEFINED swiftpm_WORKING_DIRECTORY)
+        set(swiftpm_WORKING_DIRECTORY "${SOURCE_PATH}")
+    endif()
 
-message(STATUS "Generating Xcode project from Package.swift")
-vcpkg_execute_required_process(
-    COMMAND ${SWIFT} package generate-xcodeproj
-    WORKING_DIRECTORY ${SOURCE_PATH}
-    LOGNAME "swift-generate-${TARGET_TRIPLET}"
-)
+    find_program(SWIFT NAMES swift REQUIRED)
+    message(STATUS "Generating Xcode project from Package.swift")
+    vcpkg_execute_required_process(
+        COMMAND ${SWIFT} package generate-xcodeproj ${swiftpm_PARAMS}
+        WORKING_DIRECTORY ${swiftpm_WORKING_DIRECTORY}
+        LOGNAME "${swiftpm_LOGNAME}-${TARGET_TRIPLET}"
+    )
+endfunction()
+
+function(xcodebuild_build_framework)
+    cmake_parse_arguments(PARSE_ARGV 0 xc "COPY_AFTER_BUILD" "PROJECT;FRAMEWORK;OUTPUT_DIR;LOGNAME" "PARAMS")
+    if(DEFINED xc_UNPARSED_ARGUMENTS)
+        message(FATAL_ERROR "${CMAKE_CURRENT_FUNCTION} can't handle extra arguments: ${xc_UNPARSED_ARGUMENTS}")
+    endif()
+    # required arguments
+    if(NOT DEFINED xc_PROJECT)
+        message(FATAL_ERROR "${CMAKE_CURRENT_FUNCTION} requires: PROJECT")
+    endif()
+    set(PROJECT_FILENAME "${xc_PROJECT}.xcodeproj")
+
+    if(NOT DEFINED xc_FRAMEWORK)
+        message(FATAL_ERROR "${CMAKE_CURRENT_FUNCTION} requires: FRAMEWORK")
+    endif()
+    set(FRAMEWORK_FILENAME "${xc_FRAMEWORK}.framework")
+
+    # optional arguments with default values
+    if(NOT DEFINED xc_LOGNAME)
+        set(xc_LOGNAME "build")
+    endif()
+    if(NOT DEFINED xc_WORKING_DIRECTORY)
+        set(xc_WORKING_DIRECTORY "${SOURCE_PATH}")
+    endif()
+    if(NOT DEFINED xc_OUTPUT_DIR)
+        get_filename_component(xc_OUTPUT_DIR "${xc_WORKING_DIRECTORY}/build" ABSOLUTE)
+    endif()
+
+    # expected build output location of Package.swift generated xcodeproj
+    if(VCPKG_TARGET_IS_OSX)
+        get_filename_component(OUTPUT_DIR_DBG ${xc_OUTPUT_DIR}/Debug    ABSOLUTE)
+        get_filename_component(OUTPUT_DIR_REL ${xc_OUTPUT_DIR}/Release  ABSOLUTE)
+    elseif(VCPKG_TARGET_IS_IOS)
+        get_filename_component(OUTPUT_DIR_DBG ${xc_OUTPUT_DIR}/Debug-iphoneos   ABSOLUTE)
+        get_filename_component(OUTPUT_DIR_REL ${xc_OUTPUT_DIR}/Release-iphoneos ABSOLUTE)
+        if(VCPKG_TARGET_IS_SIMULATOR)
+            get_filename_component(OUTPUT_DIR_DBG ${xc_OUTPUT_DIR}/Debug-iphonesimulator    ABSOLUTE)
+            get_filename_component(OUTPUT_DIR_REL ${xc_OUTPUT_DIR}/Release-iphonesimulator  ABSOLUTE)
+        endif()
+    else()
+        message(FATAL_ERROR "Unsupported target platform")
+    endif()
+
+    # Let's start build with xcodebuild ...
+    find_program(XCODEBUILD NAMES xcodebuild REQUIRED)
+
+    message(STATUS "Building ${TARGET_TRIPLET}-dbg")
+    vcpkg_execute_required_process(
+        COMMAND ${XCODEBUILD} -project ${PROJECT_FILENAME} -target ${xc_FRAMEWORK} -jobs ${VCPKG_CONCURRENCY} -configuration Debug ${xc_PARAMS}
+        WORKING_DIRECTORY ${xc_WORKING_DIRECTORY}
+        LOGNAME "${xc_LOGNAME}-${TARGET_TRIPLET}-dbg"
+    )
+    if(xc_COPY_AFTER_BUILD)
+        file(COPY "${OUTPUT_DIR_DBG}/${FRAMEWORK_FILENAME}" DESTINATION "${CURRENT_PACKAGES_DIR}/debug/lib")
+    endif()
+
+    message(STATUS "Building ${TARGET_TRIPLET}-rel")
+    vcpkg_execute_required_process(
+        COMMAND ${XCODEBUILD} -project ${PROJECT_FILENAME} -target ${xc_FRAMEWORK} -jobs ${VCPKG_CONCURRENCY} -configuration Release ${xc_PARAMS}
+        WORKING_DIRECTORY ${xc_WORKING_DIRECTORY}
+        LOGNAME "${xc_LOGNAME}-${TARGET_TRIPLET}-rel"
+    )
+    if(xc_COPY_AFTER_BUILD)
+        file(COPY "${OUTPUT_DIR_REL}/${FRAMEWORK_FILENAME}" DESTINATION "${CURRENT_PACKAGES_DIR}/lib")
+    endif()
+endfunction()
 
 if(VCPKG_TARGET_ARCHITECTURE STREQUAL "arm64")
     set(ARCH "arm64")
@@ -56,53 +137,58 @@ if(VCPKG_TARGET_IS_OSX)
     set(SDK macosx)
 elseif(VCPKG_TARGET_IS_IOS)
     set(SDK iphoneos)
+    if(VCPKG_TARGET_IS_SIMULATOR)
+        set(SDK iphonesimulator)
+    endif()
 else()
     message(FATAL_ERROR "Unsupported target platform")
 endif()
 
-message(STATUS "Building ${TARGET_TRIPLET}-dbg")
+# generate xcodeproject: swift-nio-ssl.xcodeproj
+find_program(SWIFT NAMES swift REQUIRED)
+message(STATUS "Using swift: ${SWIFT}")
+swiftpm_generatate_xcodeproj(LOGNAME "generate")
+
+find_program(XCODEBUILD NAMES xcodebuild REQUIRED)
+message(STATUS "Using xcodebuild: ${XCODEBUILD}")
+message(STATUS "  -sdk ${SDK} -arch ${ARCH}")
+if(DEFINED XCODEBUILD_PARAMS)
+message(STATUS "  ${XCODEBUILD_PARAMS}")
+endif()
+
+# before build, record some project info for CI environment debugging
 vcpkg_execute_required_process(
-    COMMAND ${XCODEBUILD} -project swift-nio-ssl.xcodeproj -target CNIOBoringSSL -jobs ${VCPKG_CONCURRENCY}
-                -configuration Debug -sdk ${SDK} -arch ${ARCH}
+    COMMAND ${XCODEBUILD} -project swift-nio-ssl.xcodeproj -list
     WORKING_DIRECTORY ${SOURCE_PATH}
-    LOGNAME "xcodebuild-build-${TARGET_TRIPLET}-dbg"
-)
-message(STATUS "Building ${TARGET_TRIPLET}-rel")
-vcpkg_execute_required_process(
-    COMMAND ${XCODEBUILD} -project swift-nio-ssl.xcodeproj -target CNIOBoringSSL -jobs ${VCPKG_CONCURRENCY}
-                -configuration Release -sdk ${SDK} -arch ${ARCH}
-    WORKING_DIRECTORY ${SOURCE_PATH}
-    LOGNAME "xcodebuild-build-${TARGET_TRIPLET}-rel"
+    LOGNAME "project-${TARGET_TRIPLET}"
 )
 
-set(OUT_FWK "CNIOBoringSSL.framework")
-
-file(COPY ${SOURCE_PATH}/build/Debug/${OUT_FWK}
-     DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib
+# build some targets
+xcodebuild_build_framework(PROJECT swift-nio-ssl FRAMEWORK CNIOBoringSSL
+    PARAMS -sdk ${SDK} -arch ${ARCH} ${XCODEBUILD_PARAMS}
+    LOGNAME "build1" COPY_AFTER_BUILD
 )
-file(COPY ${SOURCE_PATH}/build/Release/${OUT_FWK}
-     DESTINATION ${CURRENT_PACKAGES_DIR}/lib
+xcodebuild_build_framework(PROJECT swift-nio-ssl FRAMEWORK CNIOBoringSSLShims
+    PARAMS -sdk ${SDK} -arch ${ARCH} ${XCODEBUILD_PARAMS}
+    LOGNAME "build2" COPY_AFTER_BUILD
 )
 
 # install public/private headers
 file(GLOB headers
-    ${SOURCE_PATH}/Sources/CNIOBoringSSL/include/*.h
+    "${SOURCE_PATH}/Sources/CNIOBoringSSL/include/*.h"
 )
-file(INSTALL ${headers} DESTINATION ${CURRENT_PACKAGES_DIR}/include/CNIOBoringSSL)
+file(INSTALL ${headers} DESTINATION "${CURRENT_PACKAGES_DIR}/include/CNIOBoringSSL")
 
 file(GLOB ssl_headers
-    ${SOURCE_PATH}/Sources/CNIOBoringSSL/ssl/*.h
+    "${SOURCE_PATH}/Sources/CNIOBoringSSL/ssl/*.h"
+    # ...
 )
-file(INSTALL ${ssl_headers} DESTINATION ${CURRENT_PACKAGES_DIR}/include/ssl)
+file(INSTALL ${ssl_headers} DESTINATION "${CURRENT_PACKAGES_DIR}/include/ssl")
 
 file(GLOB crypto_headers
-    ${SOURCE_PATH}/Sources/CNIOBoringSSL/crypto/*.h
+    "${SOURCE_PATH}/Sources/CNIOBoringSSL/crypto/internal.h"
+    # ...
 )
-file(INSTALL ${crypto_headers} DESTINATION ${CURRENT_PACKAGES_DIR}/include/crypto)
-
-
-file(REMOVE_RECURSE
-    "${CURRENT_PACKAGES_DIR}/debug/lib/${OUT_FWK}/Versions/A/_CodeSignature"
-)
+file(INSTALL ${crypto_headers} DESTINATION "${CURRENT_PACKAGES_DIR}/include/crypto")
 
 file(INSTALL "${SOURCE_PATH}/LICENSE.txt" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
