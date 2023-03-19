d86f577fed428df24bdf87b34a0cfd292abdc9b1
Park DongHa <luncliff@gmail.com>
2023-03-19 22:55:03 +0900

    [xnnpack] update to latest (#93)
    * [xnnpack] update to latest(2023-03)
    
    * [xnnpack] update baseline

--- ports/xnnpack/portfile.cmake
+++ ports/xnnpack/portfile.cmake
@@ -1,19 +1,15 @@
 if(VCPKG_TARGET_IS_WINDOWS)
     vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
-    # note: `Assember` class member functions' name collides with MSVC intrinsics. Postfix '_' for those names
-    # note: Use C sources instead of ASM sources
-    list(APPEND PATCHES fix-windows-arm.patch)
 endif()
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO google/XNNPACK
-    REF dd04658ad7889be816b04995247e6f1101c9aab1 # 2022-09-09
-    SHA512 8efe5b5217cb1f3ef60974dbf671298e51a064cdd52213a51b006b89f1a4a6042c571d39289722d32bf6469870172ff481dd8bc1c73e708274fbc0cde8d8216a
+    REF bad81856dce9fea265866c87ea524afbbdd69012 # 2023-03-19
+    SHA512 eed30cc207639a32fe247f979de33c3600abbd46f98b367eaf88e4f17453ce30464cafff237eb742832411502f068af7a79f12c547ad00bc179f2972444da0d0
     HEAD_REF master
     PATCHES
         fix-cmake.patch
-        ${PATCHES}
 )
 file(COPY "${CMAKE_CURRENT_LIST_DIR}/xnnpack-config.cmake.in" DESTINATION "${SOURCE_PATH}/cmake")
 
@@ -39,6 +35,7 @@ if(VCPKG_TARGET_IS_WINDOWS)
         list(APPEND PLATFORM_OPTIONS
             -DXNNPACK_ENABLE_ARM_FP16=OFF # `__fp16` type is missing
             -DXNNPACK_ENABLE_ARM_BF16=OFF # `bfloat16_t` type is missing
+            -DXNNPACK_ENABLE_ARM_FP16_SCALAR=OFF
             -DXNNPACK_ENABLE_ARM_DOTPROD=OFF
         )
     endif()
@@ -64,11 +61,6 @@ set(USE_ASSEMBLY true)
 if(VCPKG_TARGET_IS_WINDOWS AND (VCPKG_TARGET_ARCHITECTURE MATCHES "arm"))
     set(USE_ASSEMBLY false)
 endif()
-
-vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
-    FEATURES
-        jit      XNNPACK_ENABLE_JIT
-)
 
 vcpkg_cmake_configure(
     SOURCE_PATH ${SOURCE_PATH}
