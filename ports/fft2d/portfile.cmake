8c30c4652e8c31f6e79ab6557b4f08f61c728fb6
Park DongHa <luncliff@gmail.com>
2023-05-26 15:24:59 +0900

    [fft2d] fix header installation (#105)
    * [fft2d] fix header installation
    
    * [fft2d] update baseline

--- ports/fft2d/portfile.cmake
+++ ports/fft2d/portfile.cmake
@@ -18,14 +18,16 @@ vcpkg_download_distfile(FFT2D_HEADER_1
     FILENAME tensorflow-fft2d-header-1.txt
     SHA512 afaa5baf9c26d713ae02f3a1ee06d87bedd5a902d1d2a41c0e637aa205442647ba29908f86965d22015da13ac98a5c6f16d6febe8d7040a954eadc335158047a
 )
-file(COPY ${FFT2D_HEADER_1} DESTINATION ${CURRENT_PACKAGES_DIR}/include/fft2d/fft.h)
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/include/fft2d/fft.h")
+file(INSTALL "${FFT2D_HEADER_1}" DESTINATION "${CURRENT_PACKAGES_DIR}/include/fft2d" RENAME "fft.h")
 
 vcpkg_download_distfile(FFT2D_HEADER_2
     URLS "https://raw.githubusercontent.com/tensorflow/tensorflow/v2.7.0/third_party/fft2d/fft2d.h"
     FILENAME tensorflow-fft2d-header-2.txt
     SHA512 f4c581619882a819ed116053165cc0d6f8546b6229a3c56a6077ca939d982b306c50973723c04107985fcd0fb177e76fc78f9ca774a0173e5aacbad38e3fc394
 )
-file(COPY ${FFT2D_HEADER_2} DESTINATION ${CURRENT_PACKAGES_DIR}/include/fft2d/fft2d.h)
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/include/fft2d/fft2d.h")
+file(INSTALL "${FFT2D_HEADER_2}" DESTINATION "${CURRENT_PACKAGES_DIR}/include/fft2d" RENAME "fft2d.h")
 
 file(INSTALL ${SOURCE_PATH}/readme2d.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
 file(INSTALL ${SOURCE_PATH}/readme.txt   DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT})
