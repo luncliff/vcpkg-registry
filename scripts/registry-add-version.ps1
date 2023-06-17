412e9808d68f3d27a03eacaedce75934e280c0ca
luncliff <luncliff@gmail.com>
2023-06-17 21:02:21 +0900

    scripts: descriptions for the registry scripts

--- scripts/registry-add-version.ps1
+++ scripts/registry-add-version.ps1
@@ -1,21 +1,47 @@
+<#
+.SYNOPSIS
+    PowerShell script to modify JSON files under the registry's 'versions' folder
+.DESCRIPTION
+    Runs vcpkg executable and update baseline/version JSON files.
+
+.EXAMPLE
+    PS> registry-format.ps1 -PortNmae "openssl" -VcpkgRoot $env:VCPKG_ROOT 
+.EXAMPLE
+    PS> registry-format.ps1 "openssl" -VcpkgRoot $env:VCPKG_ROOT 
+    version 1.1.1t is already in C:\vcpkg\vcpkg-registry\versions\o-\openssl1.json
+    version 1.1.1t is already in C:\vcpkg\vcpkg-registry\versions\baseline.json
+    No files were updated for openssl1
+#>
 using namespace System
-using namespace System.IO
 param
 (
-    [String]$Port,
-    [String]$VcpkgRoot = $env:VCPKG_ROOT,
-    [String]$RegistryRoot = (Join-Path -Path $env:VCPKG_ROOT "vcpkg-registry"),
-    [String]$Workspace = (Get-Location)
+    [Parameter(Position = 0, Mandatory = $true)][String]$PortName,
+    [Parameter(Mandatory = $true)][String]$VcpkgRoot,
+    [String]$RegistryRoot
 )
 
-function AddVersion([String]$root, [String]$port) {
-    ./vcpkg version
-    ./vcpkg x-add-version $port `
-        --x-builtin-ports-root="$root/ports" `
-        --x-builtin-registry-versions-dir="$root/versions"
+# VcpkgRoot
+[Boolean]$RootExists = $(Test-Path -Path $VcpkgRoot)
+if ($RootExists -eq $false) {
+    throw New-Object -TypeName ArgumentException "VcpkgRoot doesn't exist"
+}
+${env:Path} = "$VcpkgRoot;${env:Path}"
+
+# RegistryRoot
+if ($RegistryRoot.Length -eq 0) {
+    $RegistryRoot = (Join-Path -Path $VcpkgRoot "vcpkg-registry")
+    Write-Debug "RegistryRoot=${RegistryRoot}"
+}
+[Boolean]$RegistryExists = $(Test-Path -Path $RegistryRoot)
+if ($RegistryExists -eq $false) {
+    throw New-Object -TypeName ArgumentException "RegistryRoot doesn't exist"
 }
 
-# $env:Path = "$VcpkgRoot;$env:Path"
-Push-Location $VcpkgRoot
-    AddVersion $RegistryRoot $Port
-Pop-Location
+function RunAddVersion([String]$PortName, [String]$PortRoot, [String]$VersionRoot) {
+    vcpkg x-add-version $PortName `
+        --vcpkg-root="$VcpkgRoot" `
+        --x-builtin-ports-root="$PortRoot" `
+        --x-builtin-registry-versions-dir="$VersionRoot"
+}
+
+RunAddVersion -PortName $PortName -PortRoot $(Join-Path $RegistryRoot "ports") -VersionRoot $(Join-Path $RegistryRoot "versions")
--- scripts/registry-format.ps1
+++ scripts/registry-format.ps1
@@ -1,20 +1,76 @@
+<#
+.SYNOPSIS
+    PowerShell script to lint vcpkg.json under the registry's 'ports' folder
+.DESCRIPTION
+    Runs vcpkg executable and manipulates existing vcpkg.json files.
+
+.LINK
+    https://github.com/microsoft/vcpkg
+    https://github.com/microsoft/vcpkg-tool/blob/2023-06-15/include/vcpkg/vcpkgcmdarguments.h
+
+.PARAMETER VcpkgRoot
+    The root folder to find vcpkg.exe program
+.PARAMETER RegistryRoot
+    Root folder of the vcpkg registry.
+    If not provided, use "${VcpkgRoot}/vcpkg-registry"
+
+.EXAMPLE
+    PS> registry-format.ps1 -VcpkgRoot $env:VCPKG_ROOT
+    Succeeded in formatting the manifest files.
+.EXAMPLE
+    PS> registry-format.ps1 -VcpkgRoot "D:/vcpkg" -RegistryRoot $(Get-Location)
+    Succeeded in formatting the manifest files.
+.EXAMPLE
+    PS> registry-format.ps1 -VcpkgRoot $env:VCPKG_ROOT -RegistryRoot "D:/test-registry"
+    Line |
+      46 |      throw New-Object -TypeName ArgumentException "RegistryRoot doesn' …
+         |      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+         | RegistryRoot doesn't exist
+#>
 using namespace System
-using namespace System.IO
 param
 (
-    [String]$VcpkgRoot = $env:VCPKG_ROOT,
-    [String]$RegistryRoot = (Join-Path -Path $env:VCPKG_ROOT "vcpkg-registry"),
-    [String]$Workspace = (Get-Location)
+    [Parameter(Mandatory = $true)][String]$VcpkgRoot,
+    [String]$RegistryRoot
 )
 
-function RunFormat($root) {
-    ./vcpkg version
-    ./vcpkg format-manifest --all `
-        --x-builtin-ports-root="$root/ports" `
-        --x-builtin-registry-versions-dir="$root/versions"
+# VcpkgRoot
+[Boolean]$RootExists = $(Test-Path -Path $VcpkgRoot)
+if ($RootExists -eq $false) {
+    throw New-Object -TypeName ArgumentException "VcpkgRoot doesn't exist"
+}
+${env:Path} = "$VcpkgRoot;${env:Path}"
+
+# RegistryRoot
+if ($RegistryRoot.Length -eq 0) {
+    $RegistryRoot = (Join-Path -Path $VcpkgRoot "vcpkg-registry")
+    Write-Debug "RegistryRoot=${RegistryRoot}"
+}
+[Boolean]$RegistryExists = $(Test-Path -Path $RegistryRoot)
+if ($RegistryExists -eq $false) {
+    throw New-Object -TypeName ArgumentException "RegistryRoot doesn't exist"
 }
 
-# $env:Path = "$VcpkgRoot;$env:Path"
-Push-Location $VcpkgRoot
-    RunFormat $RegistryRoot
-Pop-Location
+function RunVersion() {
+    [String]$Description = $(vcpkg version)
+    Write-Debug -Message $Description
+}
+
+function RunConvert([String]$PortRoot) {
+    vcpkg format-manifest --all --convert-control `
+        --vcpkg-root="$VcpkgRoot" `
+        --x-builtin-ports-root="$PortRoot"
+}
+
+function RunFormat([String]$PortRoot, [String]$VersionRoot) {
+    Write-Debug "PortRoot=${PortRoot}"
+    Write-Debug "VersionRoot=${VersionRoot}"
+    vcpkg format-manifest --all `
+        --vcpkg-root="$VcpkgRoot" `
+        --x-builtin-ports-root="$PortRoot" `
+        --x-builtin-registry-versions-dir="$VersionRoot"
+}
+
+RunVersion
+# RunConvert -PortRoot $(Join-Path $RegistryRoot "ports") 
+RunFormat -PortRoot $(Join-Path $RegistryRoot "ports") -VersionRoot $(Join-Path $RegistryRoot "versions")
