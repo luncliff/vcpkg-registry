26b8f9ed7431d471aa81e03278a01f862036f2f3
luncliff <luncliff@gmail.com>
2023-09-27 17:23:10 +0900

    [apple-crypto] update to 3.0.0

--- ports/apple-crypto/portfile.cmake
+++ ports/apple-crypto/portfile.cmake
@@ -1,23 +1,18 @@
-if(VCPKG_TARGET_IS_IOS)
-    vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
-endif()
+vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO apple/swift-crypto
-    REF 2.2.4
-    SHA512 9b9e5e2bd038cbe8064bcbc56089219c2241bff2555abda09bc713741576e54d591a442fc6063abe2d0f78529054a7857b24179f6159b25a2447d36293951241
+    REF 3.0.0
+    SHA512 2d79147d32bfb8449726c73c8b267262cf1ddc7c18637f4168e3fd848ba71c0719b23b5683ca513260cbc537fc439e38488ae16010717303625bceb7d5edd36f
     HEAD_REF main
-    PATCHES
-        fix-cmake.patch
+    # PATCHES
+    #     fix-cmake.patch
 )
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     GENERATOR Xcode
-    OPTIONS
-        -DBUILD_SHARED_LIBS=ON
-        -DCMAKE_POSITION_INDEPENDENT_CODE=ON
 )
 vcpkg_cmake_install()
 
@@ -29,4 +24,4 @@ file(REMOVE_RECURSE
     "${CURRENT_PACKAGES_DIR}/lib/swift"
 )
 
-file(INSTALL "${SOURCE_PATH}/LICENSE.txt" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE.txt")
