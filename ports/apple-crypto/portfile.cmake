23c7f06842f86a45f545fbeff385735f1fc86b55
luncliff <luncliff@gmail.com>
2023-09-27 18:38:06 +0900

    [apple-crypto] apply patch

--- ports/apple-crypto/portfile.cmake
+++ ports/apple-crypto/portfile.cmake
@@ -6,8 +6,8 @@ vcpkg_from_github(
     REF 3.0.0
     SHA512 2d79147d32bfb8449726c73c8b267262cf1ddc7c18637f4168e3fd848ba71c0719b23b5683ca513260cbc537fc439e38488ae16010717303625bceb7d5edd36f
     HEAD_REF main
-    # PATCHES
-    #     fix-cmake.patch
+    PATCHES
+        fix-cmake.patch
 )
 
 vcpkg_cmake_configure(
