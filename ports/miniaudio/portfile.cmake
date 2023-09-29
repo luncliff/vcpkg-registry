1ee478426e1fd09a9f4bfc73dfe51c834c5d7203
luncliff <luncliff@gmail.com>
2023-09-30 00:28:10 +0900

    [miniaudio] update to 0.11.18

--- ports/miniaudio/portfile.cmake
+++ ports/miniaudio/portfile.cmake
@@ -3,8 +3,8 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO mackron/miniaudio
-    REF 0.11.17
-    SHA512 18f5cc7a77cd68567f3f44c517fb05981551f0a875f0d446ff54ad4d8ac56f106e59c935370a8c654a02507cfe01f834f5166698c13824ab1498d5879aba9e84
+    REF 0.11.18
+    SHA512 3e4a08c326f1a3e8beef962fcb525739734b22572857fe5323d2e72c3e1890edd5d26d3e1264ca5e8ad1c691fb5c312cfb9d1f09484c72353c92b20619c9f678
 )
 
 # Build as a C++ or Objective-C++. Not in C language
