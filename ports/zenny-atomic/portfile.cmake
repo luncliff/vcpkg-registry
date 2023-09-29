c63534efea5e838086f367f41b84a5e93a09b887
luncliff <luncliff@gmail.com>
2023-09-29 21:43:35 +0900

    [zenny-atomic] use 'vcpkg-cmake' host port

--- ports/zenny-atomic/portfile.cmake
+++ ports/zenny-atomic/portfile.cmake
@@ -6,10 +6,10 @@ vcpkg_from_github(
     REF cce17a78911a43e9f963c31d172b2deee56f826e
     SHA512 db540b661ac42be33829f790f0d4a7084ece20ddec829b37fa2d4d2a6a15bead7a0b0475bb31b4cba9168224b3d0c2292f9dbf1fbf3a2d2a49e9466acb4c9387
 )
-file(COPY ${CURRENT_PORT_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
+file(COPY "${CURRENT_PORT_DIR}/CMakeLists.txt" DESTINATION "${SOURCE_PATH}")
 
-vcpkg_configure_cmake(SOURCE_PATH ${SOURCE_PATH})
-vcpkg_install_cmake()
+vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}")
+vcpkg_cmake_install()
 
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
-file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
