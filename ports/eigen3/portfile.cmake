92699e4e031e63541dbff3bc128b56190068dfc3
luncliff <luncliff@gmail.com>
2024-01-21 00:00:11 +0900

    [eigen3] use master branch

--- ports/eigen3/portfile.cmake
+++ ports/eigen3/portfile.cmake
@@ -4,12 +4,9 @@ vcpkg_from_gitlab(
     GITLAB_URL https://gitlab.com
     OUT_SOURCE_PATH SOURCE_PATH
     REPO libeigen/eigen
-    REF 3.4.0
-    SHA512 ba75ecb760e32acf4ceaf27115468e65d4f77c44f8d519b5a13e7940af2c03a304ad433368cb6d55431f307c5c39e2666ab41d34442db3cf441638e51f5c3b6a
+    REF b2814d53a707f699e5c56f565847e7020654efc2
+    SHA512 82e79174b205bafa67a8bfc92ea20882ea0002738c956f59737c9ab1f54fcff322d4eda8d8b3f3ac97513102148c2fb1b01de4482a027dd3666982fb735719a7
     HEAD_REF master
-    PATCHES
-        remove_configure_checks.patch # This removes unnecessary configure checks. Eigen3 just installs headers not anything more.
-        fix-vectorized-reductions-half.patch # Remove this patch in the next update
 )
 
 if(VCPKG_TARGET_IS_ANDROID)
@@ -34,9 +31,13 @@ vcpkg_cmake_install()
 vcpkg_cmake_config_fixup()
 vcpkg_fixup_pkgconfig()
 
-file(GLOB INCLUDES "${CURRENT_PACKAGES_DIR}/include/eigen3/*")
+# file(GLOB INCLUDES "${CURRENT_PACKAGES_DIR}/include/eigen3/*")
 # Copy the eigen header files to conventional location for user-wide MSBuild integration
-file(COPY ${INCLUDES} DESTINATION "${CURRENT_PACKAGES_DIR}/include")
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include" "${CURRENT_PACKAGES_DIR}/debug/share")
+# file(COPY ${INCLUDES} DESTINATION "${CURRENT_PACKAGES_DIR}/include")
 
-file(INSTALL "${SOURCE_PATH}/COPYING.README" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+file(REMOVE_RECURSE
+    "${CURRENT_PACKAGES_DIR}/debug/include"
+    "${CURRENT_PACKAGES_DIR}/debug/share"
+)
+
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/COPYING.README")
