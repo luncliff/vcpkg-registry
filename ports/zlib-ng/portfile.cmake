2baf271cdf871253c3a95139a1cbadab650950bd
luncliff <luncliff@gmail.com>
2024-03-12 23:47:46 +0900

    [zlib-ng] update fix-cmake.patch

--- ports/zlib-ng/portfile.cmake
+++ ports/zlib-ng/portfile.cmake
@@ -4,8 +4,8 @@ vcpkg_from_github(
     REF 2.1.6
     SHA512 59ef586c09b9a63788475abfd6dd59ed602316b38f543f801bea802ff8bec8b55a89bee90375b8bbffa3bdebc7d92a00903f4b7c94cdc1a53a36e2e1fd71d13a
     HEAD_REF develop
-    # PATCHES
-    #     fix-cmake.patch
+    PATCHES
+        fix-cmake.patch
 )
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
