399b1f08423cd4477b9849f43dfc5949c7bf98a8
Park DongHa <luncliff@gmail.com>
2024-03-26 13:22:53 +0900

    [zlib-ng] Update to 2.1.6 (#184)
    * [zlib-ng] update to 2.1.6
    
    * [zlib-ng] update baseline
    
    * [zlib-ng] update fix-cmake.patch

--- ports/zlib-ng/portfile.cmake
+++ ports/zlib-ng/portfile.cmake
@@ -1,17 +1,12 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO zlib-ng/zlib-ng
-    REF 2.0.7
-    SHA512 1c19a62bb00727ac49049c299fb70060da95b5fafa448144ae4133372ec8c3da15cef6c1303485290f269b23c580696554ca0383dba3e1f9609f65c332981988
-    HEAD_REF master
+    REF 2.1.6
+    SHA512 59ef586c09b9a63788475abfd6dd59ed602316b38f543f801bea802ff8bec8b55a89bee90375b8bbffa3bdebc7d92a00903f4b7c94cdc1a53a36e2e1fd71d13a
+    HEAD_REF develop
+    PATCHES
+        fix-cmake.patch
 )
-if(VCPKG_TARGET_ARCHITECTURE STREQUAL "x64")
-    # list(APPEND ARCH_OPTIONS)
-elseif(VCPKG_TARGET_ARCHITECTURE STREQUAL "x86")
-    # list(APPEND ARCH_OPTIONS)
-elseif(VCPKG_TARGET_ARCHITECTURE STREQUAL "arm64")
-    # list(APPEND ARCH_OPTIONS)
-endif()
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
     FEATURES
@@ -21,8 +16,13 @@ vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
-        ${ARCH_OPTIONS} ${FEATURE_OPTIONS}
+        ${FEATURE_OPTIONS}
+        -DSKIP_INSTALL_ALL=OFF
+        -DSKIP_INSTALL_HEADERS=OFF
+        -DSKIP_INSTALL_LIBRARIES=OFF
+        -DSKIP_INSTALL_FILES=OFF
         -DZLIB_ENABLE_TESTS=OFF
+        -DZLIBNG_ENABLE_TESTS=OFF
         -DWITH_NEW_STRATEGIES=ON
         -DWITH_NATIVE_INSTRUCTIONS=OFF # `-march=native` breaks `check_c_source_compiles`
     OPTIONS_DEBUG
@@ -31,6 +31,11 @@ vcpkg_cmake_configure(
 vcpkg_cmake_install()
 vcpkg_copy_pdbs()
 vcpkg_fixup_pkgconfig() # zlib-ng>=2.0
+if("zlib-compat" IN_LIST FEATURES)
+    vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/ZLIB PACKAGE_NAME ZLIB)
+else()
+    vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/zlib-ng PACKAGE_NAME zlib-ng)
+endif()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share"
                     "${CURRENT_PACKAGES_DIR}/debug/include"
