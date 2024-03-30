18244813e15cb2ba02f0a50e4a8b79cba9ae0f7d
Park DongHa <luncliff@gmail.com>
2024-03-30 22:21:31 +0900

    [d3d12-transition-layer,winpixeventruntime] Support `pix` feature (#185)
    * [winpixeventruntime] create a new port
    
    * [winpixeventruntime] update baseline
    
    * ci: test winpixeventruntime
    
    * [d3d12-transition-layer] update to latest, support features
    
    * use winpixeventruntime
    
    * [d3d12-transition-layer] update baseline
    
    * ci: test d3d12-transition-layer[pix]

--- ports/d3d12-transition-layer/portfile.cmake
+++ ports/d3d12-transition-layer/portfile.cmake
@@ -5,22 +5,26 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO microsoft/D3D12TranslationLayer
-    REF 283fe2eb00d75a3e4b59d935f486fffa4eac0b4d
-    SHA512 837c8f520ffee924e11a843ab9353ed8d27ff54e2f7c746fe771e1d955637430fd351f6020594fa4b9340270d4b9c0472d37780bbe174444b05e0a48a65d7805
+    REF 1666438638012549dbaf959f921bca2aeff5c550
+    SHA512 27e6ea47fab94df198b862b7dd6e97bc120ca873c4f18e639de6f69c0e4fecd4f46f9b742ec7aac106182b9c05903e71ae641cf8f83202f4cebf02ac2200b808
     PATCHES
         fix-vcpkg.patch
     HEAD_REF master
+)
+file(REMOVE "${SOURCE_PATH}/external/d3dx12.h" "${SOURCE_PATH}/external/d3d12compatibility.h")
+
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    FEATURES
+        pix  USE_PIX
+        wdk  HAS_WDK
 )
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     WINDOWS_USE_MSBUILD
-    OPTIONS
-        -DUSE_PIX=OFF
-        -DHAS_WDK=OFF
+    OPTIONS ${FEATURE_OPTIONS}
 )
 vcpkg_cmake_install()
-vcpkg_copy_pdbs()
 
 file(REMOVE_RECURSE
     "${CURRENT_PACKAGES_DIR}/debug/include"
