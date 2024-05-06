0795856d69ef8e1827ac24e14a1898a00f6db293
luncliff <luncliff@gmail.com>
2024-05-06 21:16:20 +0900

    [xnnpack] update to 2024-05-06

--- ports/xnnpack/xnnpack-config.template.cmake
+++ ports/xnnpack/xnnpack-config.template.cmake
@@ -4,14 +4,13 @@ if(Z_XNNPACK_CONFIG_GUARD)
 endif()
 set(Z_XNNPACK_CONFIG_GUARD ON CACHE INTERNAL "Guard variable for 'xnnpack-config.cmake'")
 
+include(CMakeFindDependencyMacro)
+
 # from vcpkg, luncliff/vcpkg-registry
-if(NOT DEFINED PTHREADPOOL_LIBRARY)
-  find_library(PTHREADPOOL_LIBRARY NAMES pthreadpool REQUIRED)
-  get_filename_component(PTHREADPOOL_LIBRARY_DIR "${PTHREADPOOL_LIBRARY}" PATH)
-endif()
-if(NOT TARGET cpuinfo::cpuinfo)
-  find_package(cpuinfo CONFIG REQUIRED) # cpuinfo::cpuinfo
-endif()
+find_library(PTHREADPOOL_LIBRARY NAMES pthreadpool REQUIRED)
+get_filename_component(PTHREADPOOL_LIBRARY_DIR "${PTHREADPOOL_LIBRARY}" PATH)
+
+find_dependency(cpuinfo) # cpuinfo::cpuinfo
 
 find_path(XNNPACK_INCLUDE_DIR NAMES xnnpack.h REQUIRED)
 find_library(XNNPACK_LIBRARY NAMES XNNPACK REQUIRED)
@@ -22,19 +21,16 @@ if(XNNPACK_LIBRARY MATCHES "${CMAKE_SHARED_LIBRARY_SUFFIX}")
 endif()
 add_library(xnnpack ${XNNPACK_IMPORT_TYPE} IMPORTED)
 
+# because this is not officially supported...
+add_library(unofficial::xnnpack ALIAS xnnpack)
+
 set_target_properties(xnnpack
 PROPERTIES
-  C_STANDARD 11
+  C_STANDARD 99
   CXX_STANDARD 14
   INTERFACE_COMPILE_FEATURES "cxx_std_14"
   INTERFACE_INCLUDE_DIRECTORIES "${XNNPACK_INCLUDE_DIR}"
   INTERFACE_LINK_LIBRARIES "${PTHREADPOOL_LIBRARY};cpuinfo::cpuinfo"
+  INTERFACE_LINK_DIRECTORIES "${PTHREADPOOL_LIBRARY_DIR}"
   IMPORTED_LOCATION "${XNNPACK_LIBRARY}"
 )
-
-if(DEFINED PTHREADPOOL_LIBRARY_DIR)
-  set_target_properties(xnnpack
-  PROPERTIES
-    INTERFACE_LINK_DIRECTORIES "${PTHREADPOOL_LIBRARY_DIR}"
-  )
-endif()
