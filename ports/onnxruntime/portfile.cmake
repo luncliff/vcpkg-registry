30be8be32d1c6e773f976863122e82df6fa8c098
luncliff <luncliff@gmail.com>
2024-06-27 11:54:46 +0900

    [onnxruntime] update to 1.18.0
    * remove existing features to rework

--- ports/onnxruntime/portfile.cmake
+++ ports/onnxruntime/portfile.cmake
@@ -4,14 +4,11 @@ string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" BUILD_SHARED)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO microsoft/onnxruntime
-    REF v1.17.3
-    SHA512 f24e333ad113e15733867fae237c3495f93e373b2998508deeebb061ce9a56c444bf68fc49ae251bcc45539d0695f3ae758d73dc3c42bc01bbd7cfaa8561c793
+    REF v1.18.0
+    SHA512 2e1d724eda5635fc24f93966412c197c82ee933aaea4f4ce907b5f2ee7730c1e741f2ef4d50a2d54284fc7bd05bf104bd3c56fd4466525fcd70e63c07fbb2b16
     PATCHES
-        fix-onnxruntime-pr-19966.patch # https://github.com/microsoft/onnxruntime/pull/19966 for OpenVINO 2024.0+
         fix-cmake.patch
-        fix-sources.patch
-        fix-xnnpack.patch # todo: check xnnpack updates & tests
-        fix-clang-cl-simd-compile.patch
+        # fix-clang-cl-simd-compile.patch
         fix-llvm-rc-unicode.patch
 )
 file(COPY "${CMAKE_CURRENT_LIST_DIR}/onnxruntime_vcpkg_deps.cmake" DESTINATION "${SOURCE_PATH}/cmake/external")
@@ -70,69 +67,24 @@ vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
 )
 
 if(VCPKG_TARGET_IS_WINDOWS OR VCPKG_TARGET_IS_UWP)
-    # For some reason CUDA compiler detection is not working in WINDOWS_USE_MSBUILD
-    if(NOT ("cuda" IN_LIST FEATURES))
-        set(GENERATOR_OPTIONS WINDOWS_USE_MSBUILD)
-    endif()
+    set(GENERATOR_OPTIONS WINDOWS_USE_MSBUILD)
 elseif(VCPKG_TARGET_IS_OSX OR VCPKG_TARGET_IS_IOS)
     set(GENERATOR_OPTIONS GENERATOR Xcode)
-else()
-    set(GENERATOR_OPTIONS GENERATOR Ninja)
 endif()
 
-# todo: provide options for SIMD
-if(VCPKG_TARGET_IS_WINDOWS)
-    # target platform should be informed to activate SIMD properly
-    if(VCPKG_TARGET_ARCHITECTURE STREQUAL "x64")
-        list(APPEND ARCH_OPTIONS -DCMAKE_SYSTEM_PROCESSOR="x64")
-    elseif(VCPKG_TARGET_ARCHITECTURE STREQUAL "x86")
-        list(APPEND ARCH_OPTIONS -DCMAKE_SYSTEM_PROCESSOR="Win32")
-    elseif(VCPKG_TARGET_ARCHITECTURE STREQUAL "arm64")
-        list(APPEND ARCH_OPTIONS -DCMAKE_SYSTEM_PROCESSOR="ARM64")
-    elseif(VCPKG_TARGET_ARCHITECTURE STREQUAL "arm")
-        list(APPEND ARCH_OPTIONS -DCMAKE_SYSTEM_PROCESSOR="ARM")
-    else()
-        message(WARNING "Unexpected architecture: ${VCPKG_TARGET_ARCHITECTURE}")
-        list(APPEND ARCH_OPTIONS -DCMAKE_SYSTEM_PROCESSOR="${VCPKG_TARGET_ARCHITECTURE}")
-    endif()
-endif()
-
-if("python" IN_LIST FEATURES)
-    x_vcpkg_get_python_packages(
-        PYTHON_VERSION 3
-        PACKAGES numpy sympy # flatbuffers protobuf
-        OUT_PYTHON_VAR PYTHON3
-    )
-    get_filename_component(PYTHON_PATH "${PYTHON3}" PATH)
-else()
-    vcpkg_find_acquire_program(PYTHON3)
-    get_filename_component(PYTHON_PATH "${PYTHON3}" PATH)
-endif()
+vcpkg_find_acquire_program(PYTHON3)
+get_filename_component(PYTHON_PATH "${PYTHON3}" PATH)
 message(STATUS "Using python3: ${PYTHON3}")
 vcpkg_add_to_path(PREPEND "${PYTHON_PATH}")
-
-if("cuda" IN_LIST FEATURES)
-    # https://cmake.org/cmake/help/latest/module/FindCUDAToolkit.html
-    if(NOT DEFINED ENV{CUDA_PATH})
-        message(FATAL_ERROR "ENV{CUDA_PATH} is required. Please check the environment variable")
-    endif()
-    message(STATUS "Using CUDA: $ENV{CUDA_PATH}")
-    get_filename_component(CUDA_VERSION "$ENV{CUDA_PATH}" NAME)
-    string(REPLACE "v" "" CUDA_VERSION "${CUDA_VERSION}") # "v12.0" -> "12.0"
-    message(STATUS "  version: ${CUDA_VERSION}")
-endif()
 
 # see tools/ci_build/build.py
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}/cmake"
     ${GENERATOR_OPTIONS}
     OPTIONS
-        ${ARCH_OPTIONS}
         ${FEATURE_OPTIONS}
         -DPython_EXECUTABLE:FILEPATH=${PYTHON3}
         -DProtobuf_PROTOC_EXECUTABLE:FILEPATH=${PROTOC}
-        -DInferenceEngine_DIR:PATH=${CURRENT_INSTALLED_DIR}/share/openvino
-        -Dngraph_DIR:PATH=${CURRENT_INSTALLED_DIR}/share/openvino
         # -DProtobuf_USE_STATIC_LIBS=OFF
         -DBUILD_PKGCONFIG_FILES=ON
         -Donnxruntime_BUILD_SHARED_LIB=${BUILD_SHARED}
@@ -153,8 +105,8 @@ vcpkg_cmake_configure(
         -Donnxruntime_USE_NEURAL_SPEED=OFF
         -DUSE_NEURAL_SPEED=OFF
         # for ORT_BUILD_INFO
-        -DORT_GIT_COMMIT:STRING="56b660f36940a919295e6f1e18ad3a9a93a10bf7"
-        -DORT_GIT_BRANCH:STRING="v1.17.3"
+        -DORT_GIT_COMMIT:STRING="45737400a2f3015c11f005ed7603611eaed306a6"
+        -DORT_GIT_BRANCH:STRING="v1.18.0"
     OPTIONS_DEBUG
         -Donnxruntime_ENABLE_MEMLEAK_CHECKER=OFF
         -Donnxruntime_ENABLE_MEMORY_PROFILE=OFF
@@ -164,10 +116,7 @@ vcpkg_cmake_configure(
         onnxruntime_TENSORRT_PLACEHOLDER_BUILDER
         onnxruntime_USE_CUSTOM_DIRECTML
         onnxruntime_NVCC_THREADS
-        InferenceEngine_DIR
-        ngraph_DIR
 )
-vcpkg_cmake_build(TARGET onnxruntime LOGFILE_BASE build-onnxruntime)
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/onnxruntime PACKAGE_NAME onnxruntime)
 vcpkg_copy_pdbs()
