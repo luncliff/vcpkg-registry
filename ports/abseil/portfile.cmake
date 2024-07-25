f24f9575adeafd6fe20ee3763d276fd67f77dd08
luncliff <luncliff@gmail.com>
2024-07-26 01:26:49 +0900

    [abseil] create a new port
    * https://github.com/abseil/abseil-cpp/releases/tag/20240722.rc1

---
+++ ports/abseil/portfile.cmake
@@ -0,0 +1,34 @@
+vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+
+# https://github.com/abseil/abseil-cpp/releases/tag/20240722.rc1
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO abseil/abseil-cpp
+    REF "${VERSION}"
+    SHA512 282bcef51aeeee3257f917e7ac5ffebc9e2d2c182072c420cf7e363fc4f3228b7243433e9815f99bb9e8c224b1916183717a552f65f0ad14585733bda23067dd
+    HEAD_REF master
+)
+
+string(COMPARE EQUAL "${VCPKG_CRT_LINKAGE}" "static" USE_STATIC_RUNTIME)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    DISABLE_PARALLEL_CONFIGURE
+    OPTIONS
+        -DCMAKE_CXX_STANDARD=17
+        -DABSL_PROPAGATE_CXX_STD=ON
+        -DABSL_ENABLE_INSTALL=ON
+        -DABSL_MSVC_STATIC_RUNTIME=${USE_STATIC_RUNTIME}
+        -DABSL_BUILD_TESTING=OFF
+        -DABSL_USE_EXTERNAL_GOOGLETEST=ON
+)
+vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/absl PACKAGE_NAME absl)
+vcpkg_fixup_pkgconfig()
+
+file(REMOVE_RECURSE
+    "${CURRENT_PACKAGES_DIR}/debug/share"
+    "${CURRENT_PACKAGES_DIR}/debug/include"
+)
+
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
