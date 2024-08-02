ab13fe4dfe7e5f94ab8164585070d9f9d1d224ba
luncliff <luncliff@gmail.com>
2024-08-02 17:51:01 +0900

    [cpuinfo] try osx multiarch build

--- ports/cpuinfo/portfile.cmake
+++ ports/cpuinfo/portfile.cmake
@@ -3,6 +3,14 @@ if(VCPKG_TARGET_IS_WINDOWS)
     vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 endif()
 
+if(VCPKG_TARGET_IS_OSX)
+    list(LENGTH VCPKG_OSX_ARCHITECTURES arch_count)
+    set(OSX_ARCHS ${VCPKG_OSX_ARCHITECTURES})
+    unset(VCPKG_OSX_ARCHITECTURES)
+else()
+    set(arch_count 1)
+endif()
+
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO pytorch/cpuinfo
@@ -16,20 +24,42 @@ vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
         tools CPUINFO_BUILD_TOOLS
 )
 
-vcpkg_cmake_configure(
-    SOURCE_PATH "${SOURCE_PATH}"
-    OPTIONS
-        ${FEATURE_OPTIONS}
-        -DCPUINFO_BUILD_UNIT_TESTS=OFF
-        -DCPUINFO_BUILD_MOCK_TESTS=OFF
-        -DCPUINFO_BUILD_BENCHMARKS=OFF
-        -DCPUINFO_BUILD_PKG_CONFIG=ON
-    OPTIONS_DEBUG
-        -DCPUINFO_LOG_LEVEL=debug
-    OPTIONS_RELEASE
-        -DCPUINFO_LOG_LEVEL=default
-)
-vcpkg_cmake_install()
+macro(install_cpuinfo)
+    vcpkg_cmake_configure(
+        SOURCE_PATH "${SOURCE_PATH}"
+        OPTIONS
+            ${FEATURE_OPTIONS}
+            -DCPUINFO_BUILD_UNIT_TESTS=OFF
+            -DCPUINFO_BUILD_MOCK_TESTS=OFF
+            -DCPUINFO_BUILD_BENCHMARKS=OFF
+            -DCPUINFO_BUILD_PKG_CONFIG=ON
+        OPTIONS_DEBUG
+            -DCPUINFO_LOG_LEVEL=debug
+        OPTIONS_RELEASE
+            -DCPUINFO_LOG_LEVEL=default
+    )
+    vcpkg_cmake_install()
+endmacro()
+
+if(VCPKG_TARGET_IS_OSX AND (arch_count GREATER 1))
+    # remember original install location
+    get_filename_component(PACKAGES_DIR "${CURRENT_PACKAGES_DIR}" ABSOLUTE)
+    # build for each architecture...
+    foreach(VCPKG_TARGET_ARCHITECTURE ${OSX_ARCHS})
+        set(VCPKG_OSX_ARCHITECTURES ${VCPKG_TARGET_ARCHITECTURE})
+        set(CURRENT_PACKAGES_DIR "${PACKAGES_DIR}/${VCPKG_TARGET_ARCHITECTURE}")
+        file(REMOVE_RECURSE
+            "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg"
+            "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel"
+        )
+        install_cpuinfo()
+    endforeach()
+    get_filename_component(CURRENT_PACKAGES_DIR "${PACKAGES_DIR}" ABSOLUTE)
+    # combine ...
+    # ...
+else()
+    install_cpuinfo()
+endif()
 vcpkg_cmake_config_fixup(CONFIG_PATH share/cpuinfo PACKAGE_NAME ${PORT})
 vcpkg_fixup_pkgconfig() # pkg_check_modules(libcpuinfo)
 
