859d48891db7e2abd4942290fc5fcbae12547a25
luncliff <luncliff@gmail.com>
2024-08-06 01:27:27 +0900

    [gemmlowp] update to 2023-11-04

--- ports/gemmlowp/portfile.cmake
+++ ports/gemmlowp/portfile.cmake
@@ -1,15 +1,11 @@
-if(VCPKG_TARGET_IS_WINDOWS)
-    vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
-endif()
+vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO google/gemmlowp
-    REF e844ffd17118c1e17d94e1ba4354c075a4577b88
-    SHA512 934bc79cc951dd18bff413148f3526332685bf0980ddcc0f990ba347bafcd73725e5ffd4bd255dcaa21f0fd71698d26726e2f9f36aa8df87380048041e536e20
+    REF 16e8662c34917be0065110bfcd9cc27d30f52fdf
+    SHA512 d020c49c830f35e2568c3829d9b746285c835d3688fa77a7f08faa1bbe51e2e0fad34d5f4336f7c3fb86bc8a0afc869f34ced5464f7b3a120f3d0d34eb485ba1
     HEAD_REF master
-    PATCHES
-        fix-cmakelists.patch
 )
 
 vcpkg_cmake_configure(
@@ -20,5 +16,5 @@ vcpkg_cmake_configure(
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/${PORT}")
 
-file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
