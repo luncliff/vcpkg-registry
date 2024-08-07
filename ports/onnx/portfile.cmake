36f903a2017e71cea668df84402381703c83d694
luncliff <luncliff@gmail.com>
2024-08-07 22:59:09 +0900

    [onnx] update to 1.16.1
    * https://github.com/onnx/onnx/tree/v1.16.1

--- ports/onnx/portfile.cmake
+++ ports/onnx/portfile.cmake
@@ -3,25 +3,14 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO onnx/onnx
-    REF v1.16.0
-    SHA512 ef641447d8d6c4ed9f083793fe14a8568d6aa7b9b7e7b859a4082e9b892acd801230da2027d097ceaa0d68bbd37b2422b89bb7d1d55d5c3b5955c0f9c7c657c5
+    REF v1.16.1
+    SHA512 0ae1b36563ddeaa9947bf452eb20b83174e4c2bc4696b61768f096c401666323343fac0f699f756da99d3c29df15a0bd78fe3fa51da15f133617d7310c5b28d4
     PATCHES
         fix-cmake.patch
         support-test.patch
 )
 
 string(COMPARE EQUAL "${VCPKG_CRT_LINKAGE}" "static" USE_STATIC_RUNTIME)
-
-# ONNX_USE_PROTOBUF_SHARED_LIBS: find the library and check its file extension
-find_library(PROTOBUF_LIBPATH NAMES protobuf PATHS "${CURRENT_INSTALLED_DIR}/bin" "${CURRENT_INSTALLED_DIR}/lib" REQUIRED)
-message(STATUS "Found protobuf: ${PROTOBUF_LIBPATH}")
-
-get_filename_component(PROTOBUF_LIBNAME "${PROTOBUF_LIBPATH}" NAME)
-if(PROTOBUF_LIBNAME MATCHES "${CMAKE_SHARED_LIBRARY_SUFFIX}")
-    set(USE_PROTOBUF_SHARED ON)
-else()
-    set(USE_PROTOBUF_SHARED OFF)
-endif()
 
 find_program(PROTOC NAMES protoc
     PATHS "${CURRENT_HOST_INSTALLED_DIR}/tools/protobuf"
@@ -31,10 +20,10 @@ message(STATUS "Using protoc: ${PROTOC}")
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
     FEATURES
-        python              BUILD_ONNX_PYTHON
-        protobuf-lite       ONNX_USE_LITE_PROTO
-        test                ONNX_BUILD_TESTS
-        disable-exception           ONNX_DISABLE_EXCEPTIONS
+        test    ONNX_BUILD_TESTS
+        python  BUILD_ONNX_PYTHON
+        protobuf-lite   ONNX_USE_LITE_PROTO
+        disable-exception   ONNX_DISABLE_EXCEPTIONS
         disable-static-registration ONNX_DISABLE_STATIC_REGISTRATION
 )
 
@@ -56,9 +45,7 @@ if("python" IN_LIST FEATURES)
     get_filename_component(PYTHON_ROOT "${PYTHON_PATH}" PATH)
     find_path(pybind11_DIR NAMES pybind11Targets.cmake PATHS "${PYTHON_ROOT}/Lib/site-packages/pybind11/share/cmake/pybind11" REQUIRED)
     message(STATUS "Using pybind11: ${pybind11_DIR}")
-    list(APPEND FEATURE_OPTIONS
-        -Dpybind11_DIR:PATH=${pybind11_DIR}
-    )
+    list(APPEND FEATURE_OPTIONS "-Dpybind11_DIR:PATH=${pybind11_DIR}")
 endif()
 
 vcpkg_cmake_configure(
@@ -71,14 +58,12 @@ vcpkg_cmake_configure(
         -DONNX_VERIFY_PROTO3=ON # --protoc_path for gen_proto.py
         -DONNX_ML=ON
         -DONNX_GEN_PB_TYPE_STUBS=ON
-        -DONNX_USE_PROTOBUF_SHARED_LIBS=${USE_PROTOBUF_SHARED}
         -DONNX_USE_MSVC_STATIC_RUNTIME=${USE_STATIC_RUNTIME}
         -DONNX_BUILD_BENCHMARKS=OFF
     MAYBE_UNUSED_VARIABLES
         ONNX_USE_MSVC_STATIC_RUNTIME
         ONNX_CUSTOM_PROTOC_EXECUTABLE
 )
-
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/ONNX PACKAGE_NAME ONNX)
 if("test" IN_LIST FEATURES)
