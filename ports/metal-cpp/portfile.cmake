5fad12535e5682ba8593196f671bf5a65042bcf4
PARK DongHa <luncliff@gmail.com>
2024-08-11 01:44:41 +0900

    [metal-cpp] Update to macOS14.2_iOS17.2 (#231)
    * [metal-cpp] update to macOS14.2_iOS17.2
    
    * [metal-cpp] update baseline

--- ports/metal-cpp/portfile.cmake
+++ ports/metal-cpp/portfile.cmake
@@ -1,17 +1,23 @@
+# see https://github.com/bkaradzic/metal-cpp
 vcpkg_download_distfile(ARCHIVE
-    URLS "https://developer.apple.com/metal/cpp/files/metal-cpp_macOS13.3_iOS16.4.zip"
-    FILENAME metal-cpp_macOS13.3_iOS16.4.zip
-    SHA512 27da1cb31407bb6dae25d78dcbe1480e408968eb53983b1e616f9d609f22bfe4f91020c2968f855f55a414086b264259b7454cad33a239b946749afbcf3d770a
+    URLS "https://developer.apple.com/metal/cpp/files/metal-cpp_macOS14.2_iOS17.2.zip"
+    FILENAME metal-cpp_macOS14.2_iOS17.2.zip
+    SHA512 5d1152cda3a3b1284854f352b0e56c27c19491aa614de28db385d69e99d8b88a0186d31b373ef6db74d06dbabecf2ef8ce83a8e2d99f4bcb846ff7d58698629c
 )
 
 vcpkg_extract_source_archive_ex(
     OUT_SOURCE_PATH SOURCE_PATH
-    ARCHIVE ${ARCHIVE}
+    ARCHIVE "${ARCHIVE}"
 )
 
-file(INSTALL ${SOURCE_PATH}/Metal       DESTINATION ${CURRENT_PACKAGES_DIR}/include)
-file(INSTALL ${SOURCE_PATH}/Foundation  DESTINATION ${CURRENT_PACKAGES_DIR}/include)
-file(INSTALL ${SOURCE_PATH}/QuartzCore  DESTINATION ${CURRENT_PACKAGES_DIR}/include)
+file(INSTALL "${SOURCE_PATH}/Metal"
+             "${SOURCE_PATH}/MetalFX"
+             "${SOURCE_PATH}/Foundation"
+             "${SOURCE_PATH}/QuartzCore"
+    DESTINATION "${CURRENT_PACKAGES_DIR}/include"
+)
 
-file(INSTALL ${SOURCE_PATH}/LICENSE.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
-file(INSTALL ${SOURCE_PATH}/README.md   DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT})
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" "${SOURCE_PATH}/README.md"
+    DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
+)
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE.txt")
