1f8e504e846b772fcae5f813d833ce7f8e7bf143
luncliff <luncliff@gmail.com>
2024-09-01 21:43:13 +0900

    [fp16] update to 2024-06-20

--- ports/fp16/portfile.cmake
+++ ports/fp16/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Maratyszcza/fp16
-    REF 581ac1c79dd9d9f6f4e8b2934e7a55c7becf0799
-    SHA512 a7898d9fe4ae183562c87b3e61865b1166dd45ab342f6874a42ec8123f8cf41c0074df0bce7696d378c364ff5f363d33942f12c63d804efacb7423b64f3c10f4
+    REF 98b0a46bce017382a6351a19577ec43a715b6835
+    SHA512 d3ae46b5b0c944f1d8dcfbb90689266f4abaff3e0b5ef338d5d79193367f06d1bfbb9ad85a5a7685a894daeee6dc73fc5d73631718be1379cc6918655a0289aa
 )
 
 file(INSTALL    "${SOURCE_PATH}/include/fp16.h"
@@ -10,7 +10,7 @@ file(INSTALL    "${SOURCE_PATH}/include/fp16.h"
 )
 file(INSTALL    "${SOURCE_PATH}/include/fp16/fp16.h"
                 "${SOURCE_PATH}/include/fp16/bitcasts.h"
-                "${SOURCE_PATH}/include/fp16/psimd.h"
+                "${SOURCE_PATH}/include/fp16/macros.h"
     DESTINATION "${CURRENT_PACKAGES_DIR}/include/fp16"
 )
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
