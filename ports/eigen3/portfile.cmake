c2c71dcafc13e32afc4bedffbafbe083425d602f
luncliff <luncliff@gmail.com>
2024-09-14 20:24:25 +0900

    [eigen3] use 3.4 branch, latest commit
    * https://gitlab.com/libeigen/eigen/-/commit/9df21dc8b4b576a7aa5c0094daa8d7e8b8be60f0

--- ports/eigen3/portfile.cmake
+++ ports/eigen3/portfile.cmake
@@ -1,13 +1,15 @@
-vcpkg_buildpath_length_warning(37)
+vcpkg_buildpath_length_warning(40)
 
 vcpkg_from_gitlab(
     GITLAB_URL https://gitlab.com
     OUT_SOURCE_PATH SOURCE_PATH
     REPO libeigen/eigen
-    REF ddb163ffb1d39cab299a584f2965007b6d7e755d
-    SHA512 e30adf2b0f5faa6fe5d22d3d22311933415f267db758b4a895d3a2917313bb138b26983c7763612e22e05271f5dc7b4b0ad7c4bc44dff065ca5c619a863357d1
-    HEAD_REF master
+    REF 9df21dc8b4b576a7aa5c0094daa8d7e8b8be60f0
+    SHA512 c79724f5e0a97f9e96601248acab804fb5e7bda77f6c05d1b6b933b60c35d1f644214640bec74bac95104a74677b391624c7e0cc202ec2759ba74170bd5d34e8
+    HEAD_REF 3.4
 )
+# check ${SOURCE_PATH}/Eigen/src/Core/Util/Macros.h
+#  EIGEN_WORLD_VERSION, EIGEN_MAJOR_VERSION, EIGEN_MINOR_VERSION
 
 if(VCPKG_TARGET_IS_ANDROID)
     list(APPEND PLATFORM_OPTIONS -DCMAKE_Fortran_COMPILER=OFF)
@@ -17,23 +19,20 @@ vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         -DBUILD_TESTING=OFF
+        -DEIGEN_BUILD_DOC=OFF
         -DEIGEN_BUILD_PKGCONFIG=ON
         ${PLATFORM_OPTIONS}
     OPTIONS_RELEASE
-        -DCMAKEPACKAGE_INSTALL_DIR=${CURRENT_PACKAGES_DIR}/share/eigen3
+        -DCMAKEPACKAGE_INSTALL_DIR=${CURRENT_PACKAGES_DIR}/lib/cmake/${PORT}
         -DPKGCONFIG_INSTALL_DIR=${CURRENT_PACKAGES_DIR}/lib/pkgconfig
     OPTIONS_DEBUG
-        -DCMAKEPACKAGE_INSTALL_DIR=${CURRENT_PACKAGES_DIR}/debug/share/eigen3
+        -DCMAKEPACKAGE_INSTALL_DIR=${CURRENT_PACKAGES_DIR}/debug/lib/cmake/${PORT}
         -DPKGCONFIG_INSTALL_DIR=${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig
 )
 
 vcpkg_cmake_install()
-vcpkg_cmake_config_fixup()
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/${PORT} PACKAGE_NAME Eigen3)
 vcpkg_fixup_pkgconfig()
-
-# file(GLOB INCLUDES "${CURRENT_PACKAGES_DIR}/include/eigen3/*")
-# Copy the eigen header files to conventional location for user-wide MSBuild integration
-# file(COPY ${INCLUDES} DESTINATION "${CURRENT_PACKAGES_DIR}/include")
 
 file(REMOVE_RECURSE
     "${CURRENT_PACKAGES_DIR}/debug/include"
