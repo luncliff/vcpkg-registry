e3e01f0cf05db08218dfc57f52c9b45d4240637e
luncliff <luncliff@gmail.com>
2024-09-28 00:10:42 +0900

    [ml-dtypes] update checksum

--- ports/ml-dtypes/portfile.cmake
+++ ports/ml-dtypes/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO jax-ml/ml_dtypes
     REF v0.5.0
-    SHA512 0
+    SHA512 20b2677c1e69b9ea298ac7d8ccfee3f0d19b847a805efa183ba04845e1a1697e9030abe00d6e8a4d46981df14c8d5986bcbc3b4e0f36eef2aee1cd04e3996bfe
 )
 file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" DESTINATION "${SOURCE_PATH}/ml_dtypes")
 
