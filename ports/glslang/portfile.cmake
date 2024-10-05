235a506e87aeb5990299bc330ea08893ce5f4559
luncliff <luncliff@gmail.com>
2024-10-05 23:57:33 +0900

    [glslang] install private headers

--- ports/glslang/portfile.cmake
+++ ports/glslang/portfile.cmake
@@ -6,6 +6,8 @@ vcpkg_from_github(
     REF ${VERSION}
     SHA512 b246c6f280891b7c9b6cd0b5e85e03ccf1fe173cdfc40e566339a5698176cbcfe23eb7aeaba277f071222d76b9f2a00376d790d4d604aedad82e6196fab7fc70
     HEAD_REF main
+    PATCHES
+        private-headers.patch
 )
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
@@ -20,16 +22,16 @@ vcpkg_add_to_path("${PYTHON_PATH}")
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
-      ${FEATURE_OPTIONS}
-      -DBUILD_EXTERNAL=OFF
-      -DENABLE_HLSL=${VCPKG_TARGET_IS_WINDOWS}
-      -DENABLE_SPIRV=ON
-      -DENABLE_RTTI=OFF
-      -DENABLE_EXCEPTIONS=OFF
-      -DENABLE_OPT=OFF # requires spirv-tools
-      -DENABLE_GLSLANG_JS=OFF
-      -DGLSLANG_TESTS=OFF
-      -DGLSLANG_ENABLE_INSTALL=ON
+        ${FEATURE_OPTIONS}
+        -DBUILD_EXTERNAL=OFF
+        -DENABLE_HLSL=${VCPKG_TARGET_IS_WINDOWS}
+        -DENABLE_SPIRV=ON
+        -DENABLE_RTTI=OFF
+        -DENABLE_EXCEPTIONS=OFF
+        -DENABLE_OPT=OFF # requires spirv-tools
+        -DENABLE_GLSLANG_JS=OFF
+        -DGLSLANG_TESTS=OFF
+        -DGLSLANG_ENABLE_INSTALL=ON
 )
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/glslang)
@@ -39,7 +41,7 @@ if("tools" IN_LIST FEATURES)
 endif()
 
 file(REMOVE_RECURSE
-  "${CURRENT_PACKAGES_DIR}/debug/include"
-  "${CURRENT_PACKAGES_DIR}/debug/share"
+    "${CURRENT_PACKAGES_DIR}/debug/include"
+    "${CURRENT_PACKAGES_DIR}/debug/share"
 )
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE.txt")
