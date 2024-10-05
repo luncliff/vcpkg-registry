ca815fdc387d0675b202d8b088253ac0ab348cd5
PARK DongHa <luncliff@gmail.com>
2024-10-05 21:08:14 +0900

    [opencl-on-dx12] Update to v1.2404.1.0 (#272)
    * [opencl-on-dx12] update to 1.2404.1.0
    
    * https://github.com/microsoft/OpenCLOn12/releases/tag/v1.2404.1.0
    
    * [opencl-on-dx12] more patches
    
    * [opencl-on-dx12] disconnect with D3D12TranslationLayer
    
    * use embedded sources instead of public port

--- ports/opencl-on-dx12/portfile.cmake
+++ ports/opencl-on-dx12/portfile.cmake
@@ -1,23 +1,14 @@
 vcpkg_check_linkage(ONLY_DYNAMIC_LIBRARY)
 
-# note: The URL is using 'master' branch. This is intended, so the port can detect some changes...
-vcpkg_download_distfile(MS_TELEMETRY_H_PATH
-    URLS "https://raw.githubusercontent.com/microsoft/winget-cli/master/src/AppInstallerSharedLib/Public/Telemetry/MicrosoftTelemetry.h"
-    FILENAME MicrosoftTelemetry.h
-    SHA512 33d229ee1e7eb9d176629d94eaa24a4ec1deb384a8b80a84834b5ca58bd0bf814dfd6051e29909e52cc293587c3edcfcf307873c43ec5bd8abb84eafa929dfd9
-)
-# file(INSTALL "${MS_TELEMETRY_H_PATH}" DESTINATION "${CURRENT_PACKAGES_DIR}/include/Telemetry")
-
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO microsoft/OpenCLOn12
-    REF b9cef1443007ed3cd4e39a03666a539f91e159e5
-    SHA512 13aa2abe8ff0e3f14ed481bd45d8741d0783a40094ea1e70280414751f40784b4c82b9da60224bda51bf3d5c491b4471242118bc7ecf42b6497a55e9276afde7
+    REF v1.2404.1.0
+    SHA512 7053cee9db381b55bab74729fa445b485e70f6c71f3358824ffae5aa4dfc6e869825196c029d94582a8b1d88029a508e7e38a24ffe9adafb242725a790f7f3e5
     PATCHES
-        fix-vcpkg.patch
+        fix-cmake.patch
     HEAD_REF master
 )
-file(COPY "${MS_TELEMETRY_H_PATH}" DESTINATION "${SOURCE_PATH}/external")
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
@@ -28,7 +19,9 @@ vcpkg_cmake_configure(
 vcpkg_cmake_install()
 vcpkg_copy_pdbs()
 
+set(VCPKG_POLICY_EMPTY_INCLUDE_FOLDER enabled)
 file(REMOVE_RECURSE
+    "${CURRENT_PACKAGES_DIR}/include"
     "${CURRENT_PACKAGES_DIR}/debug/include"
     "${CURRENT_PACKAGES_DIR}/debug/share"
 )
