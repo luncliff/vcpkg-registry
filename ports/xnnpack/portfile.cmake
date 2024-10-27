3d061d426b2439d2a52f5d1a9b9189d4615252ed
luncliff <luncliff@gmail.com>
2024-10-28 03:24:11 +0900

    [xnnpack] update to 2024-11-09
    * https://github.com/google/XNNPACK/commit/4ea82e595b36106653175dcb04b2aa532660d0d8
    * support test/benchmark build

--- ports/xnnpack/portfile.cmake
+++ ports/xnnpack/portfile.cmake
@@ -2,51 +2,42 @@ if(VCPKG_TARGET_IS_WINDOWS)
     vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 endif()
 
-# for onnxruntime, using just before https://github.com/google/XNNPACK/commit/142aceb06d509b57d02ddc2a9558ab35eacbb6fb
-
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO google/XNNPACK
-    REF 92c6254f6a2a57f394248f66271a77f4f48a27aa # 2024-05-06
-    SHA512 71fe88ba951ff353288f07ae2119301d046750c850b268eb01435816848adfd9a8f5d701d3ce134fe9b3178c4f70410fad5e5ed2521292f7c17320a904d3ac7a
+    REF 4ea82e595b36106653175dcb04b2aa532660d0d8 # 2024-11-09
+    SHA512 b9e711e1c69a24e347f64ee21c60a609bff25e36153216382acd3d431e466b127eea81b1d80f00f84699a426c44f8a3566d9b95d0e87079c34f527e05c2db787
     HEAD_REF master
     PATCHES
         fix-cmake.patch
 )
-file(COPY "${CMAKE_CURRENT_LIST_DIR}/xnnpack-config.template.cmake" DESTINATION "${SOURCE_PATH}/cmake")
 
 if(VCPKG_TARGET_IS_WINDOWS)
     # see https://docs.microsoft.com/en-us/cpp/intrinsics/arm64-intrinsics?view=msvc-170
     # see https://github.com/google/XNNPACK/blob/master/scripts/build-windows-arm64.cmd
+    # see ${SOURCE_PATH}/scripts/build-windows-arm64.cmd
     if(VCPKG_TARGET_ARCHITECTURE MATCHES "arm")
         list(APPEND PLATFORM_OPTIONS
             -DXNNPACK_ENABLE_ARM_FP16_SCALAR=OFF
             -DXNNPACK_ENABLE_ARM_BF16=OFF # `bfloat16_t` type is missing
-            -DXNNPACK_ENABLE_ARM_FP16_VECTOR=ON # `__fp16` type is missing
+            # -DXNNPACK_ENABLE_ARM_FP16_VECTOR=ON # `__fp16` type is missing
         )
     endif()
 elseif(VCPKG_TARGET_IS_ANDROID)
-    # see https://github.com/google/XNNPACK/blob/master/scripts/build-android-armv7.sh
+    # see ${SOURCE_PATH}/scripts/build-android-armv7.sh
     if(VCPKG_TARGET_ARCHITECTURE STREQUAL "arm")
         list(APPEND PLATFORM_OPTIONS -DXNNPACK_ENABLE_ARM_BF16=OFF)
     endif()
-elseif(VCPKG_TARGET_IS_IOS)
-    if(VCPKG_TARGET_ARCHITECTURE STREQUAL "arm")
-        set(IOS_ARCH "armv7")
-    elseif(VCPKG_TARGET_ARCHITECTURE STREQUAL "arm64")
-        set(IOS_ARCH "arm64")
-    elseif(VCPKG_TARGET_ARCHITECTURE STREQUAL "x64")
-        set(IOS_ARCH "x86_64")
-    else()
-        message(FATAL_ERROR "Unexpected VCPKG_TARGET_ARCHITECTURE")
-    endif()
-    list(APPEND PLATFORM_OPTIONS -DIOS_ARCH=${IOS_ARCH})
 endif()
 
-set(USE_ASSEMBLY true)
-if(VCPKG_TARGET_IS_WINDOWS AND (VCPKG_TARGET_ARCHITECTURE MATCHES "arm"))
-    set(USE_ASSEMBLY false)
-endif()
+vcpkg_find_acquire_program(PYTHON3)
+message(STATUS "Using python3: ${PYTHON3}")
+
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    FEATURES
+        test    XNNPACK_BUILD_TESTS
+        test    XNNPACK_BUILD_BENCHMARKS
+)
 
 vcpkg_cmake_configure(
     SOURCE_PATH ${SOURCE_PATH}
@@ -55,14 +46,15 @@ vcpkg_cmake_configure(
         ${FEATURE_OPTIONS}
         ${PLATFORM_OPTIONS}
         -DXNNPACK_USE_SYSTEM_LIBS=ON
-        -DXNNPACK_ENABLE_ASSEMBLY=${USE_ASSEMBLY}
+        "-DCPUINFO_SOURCE_DIR:PATH=${CURRENT_INSTALLED_DIR}"
+        "-DPTHREADPOOL_SOURCE_DIR:PATH=${CURRENT_INSTALLED_DIR}"
+        "-DFXDIV_SOURCE_DIR:PATH=${CURRENT_INSTALLED_DIR}"
+        -DXNNPACK_ENABLE_ASSEMBLY=ON
         -DXNNPACK_ENABLE_MEMOPT=ON
         -DXNNPACK_ENABLE_SPARSE=ON
-        -DXNNPACK_BUILD_TESTS=OFF
-        -DXNNPACK_BUILD_BENCHMARKS=OFF
+        "-DPython_EXECUTABLE:FILEPATH=${PYTHON3}"
 )
 vcpkg_cmake_install()
-vcpkg_cmake_config_fixup(CONFIG_PATH share PACKAGE_NAME xnnpack)
 
 file(REMOVE_RECURSE
     "${CURRENT_PACKAGES_DIR}/debug/include"
