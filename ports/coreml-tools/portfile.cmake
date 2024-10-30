1e8aa24d0d0f5c8ab88d98ff2c491347ed88df6f
luncliff <luncliff@gmail.com>
2024-10-31 01:10:01 +0900

    [coreml-tools] update portfile,patches

--- ports/coreml-tools/portfile.cmake
+++ ports/coreml-tools/portfile.cmake
@@ -2,19 +2,17 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO apple/coremltools
-    REF 7.2
-    SHA512 0b86c3a424376a432985d327935439ce215257201372d781f710ea4177b94bb404c315fb3a00e4efa2d8c4c8693fe97ec0f969d62c2e86f465a92f1c8074d158
+    REF 8.0
+    SHA512 ed1c5140d8b613632dcc408b789176ec95c11e3e1a504df92d1f7930e79e6eb9de8a42c18cf19bb115e52d5c4c64ed22e61e6bb1e8d97ebb8291a6c1e8c42920
     HEAD_REF master
     PATCHES
         fix-cmake.patch
         fix-sources.patch
 )
-file(REMOVE_RECURSE "${SOURCE_PATH}/deps")
+file(REMOVE_RECURSE "${SOURCE_PATH}/deps" "${SOURCE_PATH}/mlmodel/build/format")
+file(CREATE_LINK "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel/mlmodel/format" "${SOURCE_PATH}/mlmodel/build/format" SYMBOLIC)
 
-find_program(PROTOC NAMES protoc
-    PATHS "${CURRENT_HOST_INSTALLED_DIR}/tools/protobuf"
-    REQUIRED NO_DEFAULT_PATH NO_CMAKE_PATH
-)
+find_program(PROTOC NAMES protoc PATHS "${CURRENT_HOST_INSTALLED_DIR}/tools/protobuf" REQUIRED NO_DEFAULT_PATH NO_CMAKE_PATH)
 message(STATUS "Using protoc: ${PROTOC}")
 
 # see ${SOURCE_PATH}/setup.py
@@ -27,26 +25,23 @@ x_vcpkg_get_python_packages(
 function(get_python_version PYTHON OUT_VERSION)
     execute_process(
         COMMAND "${PYTHON}" --version
-        OUTPUT_VARIABLE output
-        OUTPUT_STRIP_TRAILING_WHITESPACE
+        OUTPUT_VARIABLE output OUTPUT_STRIP_TRAILING_WHITESPACE
     )
-    string(REGEX MATCH "([0-9]+\\.[0-9]+\\.[0-9]+)" python_version "${output}")
-    set(${OUT_VERSION} ${python_version} PARENT_SCOPE)
+    string(REGEX MATCH "([0-9]+\\.[0-9]+\\.[0-9]+)" output "${output}")
+    set(${OUT_VERSION} "${output}" PARENT_SCOPE)
 endfunction()
 get_python_version("${PYTHON3}" PYTHON_VERSION)
-
-message(STATUS "Using python3: ${PYTHON3} ${PYTHON_VERSION}")
 
 function(get_python_site_packages PYTHON OUT_PATH)
     execute_process(
         COMMAND "${PYTHON}" -c "import site; print(site.getsitepackages()[0])"
-        OUTPUT_VARIABLE output
-        OUTPUT_STRIP_TRAILING_WHITESPACE
+        OUTPUT_VARIABLE output OUTPUT_STRIP_TRAILING_WHITESPACE
     )
     set(${OUT_PATH} "${output}" PARENT_SCOPE)
 endfunction()
-
 get_python_site_packages("${PYTHON3}" SITE_PACKAGES_DIR)
+
+message(STATUS "Using python3: ${PYTHON3} ${PYTHON_VERSION}")
 message(STATUS "  site-packages: ${SITE_PACKAGES_DIR}")
 
 if(VCPKG_CROSSCOMPILING)
@@ -61,18 +56,16 @@ endif()
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
-    # GENERATOR Xcode
     OPTIONS
-        -DProtobuf_PROTOC_EXECUTABLE:FILEPATH=${PROTOC}
-        -DPython3_EXECUTABLE:FILEPATH=${PYTHON3}
-        -DPython3_ROOT_DIR:PATH="${PYTHON_ROOT}"
-        -Dpybind11_DIR:PATH="${SITE_PACKAGES_DIR}/pybind11/share/cmake/pybind11"
+        "-DProtobuf_PROTOC_EXECUTABLE:FILEPATH=${PROTOC}"
+        "-DPYTHON_EXECUTABLE:FILEPATH=${PYTHON3}"
+        "-Dpybind11_DIR:PATH=${SITE_PACKAGES_DIR}/pybind11/share/cmake/pybind11"
         -DCMAKE_CROSSCOMPILING=${VCPKG_CROSSCOMPILING}
         -DOVERWRITE_PB_SOURCE=ON
 )
-vcpkg_cmake_build(TARGET protosrc   LOGFILE_BASE build-protosrc)
+vcpkg_cmake_build(TARGET protosrc LOGFILE_BASE build-protosrc)
 if(VCPKG_TARGET_IS_OSX)
-    vcpkg_cmake_build(TARGET enumgen   LOGFILE_BASE build-enumgen)
+    vcpkg_cmake_build(TARGET enumgen LOGFILE_BASE build-enumgen)
 endif()
 vcpkg_cmake_install()
 
@@ -89,8 +82,8 @@ file(GLOB proto_files
     "${SOURCE_PATH}/mlmodel/format/*.proto"
 )
 file(GLOB generated_headers
-    "${SOURCE_PATH}/mlmodel/build/format/*.h" # *.pb.h
+    "${SOURCE_PATH}/mlmodel/build/format/*.h" # *.pb.h and *_enum.h
 )
 file(INSTALL ${proto_files} ${generated_headers} DESTINATION "${CURRENT_PACKAGES_DIR}/include/mlmodel/format")
 
-file(INSTALL "${SOURCE_PATH}/LICENSE.txt" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE.txt")
