6bf286e66e2f5047166c0deed79f6016e13599b4
luncliff <luncliff@gmail.com>
2025-02-05 22:49:11 +0900

    [llama-cpp] update to b4644
    * https://github.com/ggerganov/llama.cpp/releases/tag/b4644

--- ports/llama-cpp/portfile.cmake
+++ ports/llama-cpp/portfile.cmake
@@ -1,104 +1,92 @@
 if(VCPKG_TARGET_IS_WINDOWS)
-    vcpkg_check_linkage(ONLY_DYNAMIC_LIBRARY)
+    vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 endif()
+set(VCPKG_POLICY_DLLS_IN_STATIC_LIBRARY enabled) # there are some python scripts
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO ggerganov/llama.cpp
-    REF b1695 # commit 925e5584a058afb612f9c20bc472c130f5d0f891
-    SHA512 3f50216030fe022dbfdcdb7c96765bdf2f1995a4672e7ffbca672fc5f502149d47146428e3f5cdd4799724011b29941826cf66b9178337cce05b8aa0b5292f1c
+    REF "b${VERSION}"
+    SHA512 d939bb35e492dba06068e49dd0b28e8352fcbf598c1d7198fdedb405893b4c3a90b3709f0c57e57ffe3e92860d7c5ad21a2cc1129d50c07f27470b8b37d87183
     HEAD_REF master
+    PATCHES
+        fix-cmake-ggml.patch
 )
 
-vcpkg_find_acquire_program(PKGCONFIG)
-message(STATUS "Using pkgconfig: ${PKGCONFIG}")
+vcpkg_find_acquire_program(GIT)
+message(STATUS "Using git: ${GIT}")
 
-# check https://cmake.org/cmake/help/latest/module/FindBLAS.html#blas-lapack-vendors
-if(VCPKG_TARGET_IS_WINDOWS)
-    list(APPEND BLAS_OPTIONS -DLLAMA_BLAS_VENDOR=OpenBLAS)
-elseif(VCPKG_TARGET_IS_OSX OR VCPKG_TARGET_IS_IOS)
-    list(APPEND BLAS_OPTIONS -DLLAMA_BLAS_VENDOR=Apple)
-else()
-    # todo: Intel MKL, ARM, ACML, etc...
-    list(APPEND BLAS_OPTIONS -DLLAMA_BLAS_VENDOR=Generic)
-endif()
-
-if(VCPKG_TARGET_ARCHITECTURE STREQUAL "x64")
-    list(APPEND ARCH_OPTIONS
-        -DLLAMA_AVX512=ON -DLLAMA_AVX512_VBMI=ON -DLLAMA_AVX512_VNNI=ON
-        # -DLLAMA_AVX2=ON
-        # -DLLAMA_AVX=ON
-    )
-endif()
-
+# for BLAS, see https://cmake.org/cmake/help/latest/module/FindBLAS.html#blas-lapack-vendors
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
     FEATURES
-        cublas  LLAMA_CUBLAS
-        cublas  LLAMA_CUDA_F16
-        clblast LLAMA_CLBLAST
-        mpi     LLAMA_MPI
-        test    LLAMA_BUILD_TESTS
+        server  LLAMA_BUILD_SERVER
+        server  LLAMA_BUILD_EXAMPLES
+        cuda    LLAMA_CUBLAS
+        cuda    GGML_CUDA
+        cuda    GGML_CUDA_GRAPHS_DEFAULT
+        opencl  GGML_OPENCL
+        openmp  GGML_OPENMP
+        vulkan  GGML_VULKAN
+        vulkan  GGML_VULKAN_CHECK_RESULTS
+        # vulkan  GGML_VULKAN_VALIDATE 
 )
 
 string(COMPARE EQUAL "${VCPKG_CRT_LINKAGE}" "static" USE_STATIC)
 
-if(VCPKG_TARGET_IS_OSX OR VCPKG_TARGET_IS_IOS)
-    set(TARGET_IS_APPLE ON)
-else()
-    set(TARGET_IS_APPLE OFF)
+if(VCPKG_TARGET_ARCHITECTURE STREQUAL "x86")
+    list(APPEND ARCH_OPTIONS
+        -DGGML_AVX2=ON
+        -DGGML_AVX=ON
+        -DGGML_AVX512=ON
+        -DGGML_AVX512_VBMI=ON
+        -DGGML_AVX512_VNNI=ON
+    )
+endif()
+if(VCPKG_TARGET_ARCHITECTURE STREQUAL "arm64")
+    list(APPEND ARCH_OPTIONS
+        -DGGML_AARCH64=ON
+    )
 endif()
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
-        ${ARCH_OPTIONS}
         ${FEATURE_OPTIONS}
-        -DLLAMA_ACCELERATE=${TARGET_IS_APPLE}
-        -DLLAMA_METAL=${TARGET_IS_APPLE}
-        -DLLAMA_STATIC=${USE_STATIC}
-        -DLLAMA_BLAS=ON
-        ${BLAS_OPTIONS}
-        -DPKG_CONFIG_EXECUTABLE:FILEPATH="${PKGCONFIG}"
-        -DBUILD_COMMIT:STRING="99115f3fa654b593099c6719ad30e3f54ce231e1"
-        -DBUILD_NUMBER:STRING="1695"
+        ${ARCH_OPTIONS}
+        # see cmake/build-info.cmake
+        "-DGIT_EXECUTABLE:FILEPATH=${GIT}"
+        "-DBUILD_NUMBER=${VERSION}"
+        "-DBUILD_COMMIT:STRING=d774ab3acc4fee41fbed6dbfc192b57d5f79f34b"
+        # ${SOURCE_PATH}/CMakeLists.txt
+        -DLLAMA_STANDALONE=ON
+        -DLLAMA_CURL=ON
+        -DLLAMA_LLGUIDANCE=OFF
+        -DLLAMA_ALL_WARNINGS=OFF
+        -DLLAMA_BUILD_TESTS=OFF
+        # ${SOURCE_PATH}/ggml/CMakeLists.txt
+        -DGGML_STANDALONE=ON
+        -DGGML_BUILD_NUMBER=${VERSION}
+        -DGGML_LLAMAFILE_DEFAULT=OFF
+        -DGGML_FATAL_WARNINGS=OFF
+        -DGGML_CPU_ALL_VARIANTS=OFF
+        -DGGML_BACKEND_DL=OFF
+        -DGGML_METAL_USE_BF16=ON
+        -DGGML_METAL_EMBED_LIBRARY=ON
+        -DGGML_OPENMP=OFF
+        -DGGML_BUILD_TESTS=OFF
+        -DGGML_BUILD_EXAMPLES=OFF
     OPTIONS_RELEASE
-        -DLLAMA_METAL_NDEBUG=ON
+        -DGGML_METAL_NDEBUG=ON
 )
-vcpkg_cmake_build(TARGET "llama" LOGFILE_BASE build-llama)
 vcpkg_cmake_install()
-vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/Llama" PACKAGE_NAME "Llama")
-vcpkg_copy_pdbs()
 
-vcpkg_copy_tools(TOOL_NAMES
-    baby-llama beam-search benchmark convert-llama2c-to-ggml embedding llama-bench
-    main perplexity quantize-stats quantize save-load-state server simple speculative train-text-from-scratch    
-    batched-bench batched export-lora finetune infill llava-cli lookahead lookup parallel tokenize
-    AUTO_CLEAN
-)
-if("test" IN_LIST FEATURES)
-    vcpkg_copy_tools(TOOL_NAMES
-        test-grad0 test-grammar-parser test-llama-grammar test-quantize-fns test-quantize-perf
-        test-sampling test-tokenizer-0-falcon test-tokenizer-0-llama test-tokenizer-1-llama
-        test-backend-ops test-rope test-tokenizer-1-bpe
-        AUTO_CLEAN
-    )
-endif()
-
-file(INSTALL "${SOURCE_PATH}/llama.h" DESTINATION "${CURRENT_PACKAGES_DIR}/include")
-
-file(INSTALL    "${CURRENT_PACKAGES_DIR}/bin/convert.py"
-                "${CURRENT_PACKAGES_DIR}/bin/convert-lora-to-ggml.py"
-    DESTINATION "${CURRENT_PACKAGES_DIR}/tools/${PORT}"
-)
+vcpkg_fixup_pkgconfig()
+vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/ggml" PACKAGE_NAME "ggml" DO_NOT_DELETE_PARENT_CONFIG_PATH)
+vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/llama" PACKAGE_NAME "llama")
 
 file(REMOVE_RECURSE
     "${CURRENT_PACKAGES_DIR}/debug/include"
-)
-file(REMOVE
-    "${CURRENT_PACKAGES_DIR}/bin/convert.py"
-    "${CURRENT_PACKAGES_DIR}/debug/bin/convert.py"
-    "${CURRENT_PACKAGES_DIR}/bin/convert-lora-to-ggml.py"
-    "${CURRENT_PACKAGES_DIR}/debug/bin/convert-lora-to-ggml.py"
+    "${CURRENT_PACKAGES_DIR}/debug/share"
 )
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
