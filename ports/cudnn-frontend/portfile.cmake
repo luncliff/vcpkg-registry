9008772167632099ef7dc71355a0b70999210d30
PARK DongHa <luncliff@gmail.com>
2025-03-29 02:08:08 +0900

    [nvidia-cudnn-frontend,cudnn-frontend] create a proxy for nvidia-cudnn-frontend, update to v1.11.0 (#347)
    * [cudnn-frontend] create a new port with v1.11.0
    
    * https://github.com/NVIDIA/cudnn-frontend/releases/tag/v1.11.0
    
    * [cudnn-frontend] update baseline
    
    * [cudnn-frontend] connect to 'nvidia-cudnn-frontend'
    
    * update baseline
    
    * [nvidia-cunn-frontend] support find_package
    
    * [cudnn-frontend] remove unused file
    
    * update baseline

---
+++ ports/cudnn-frontend/portfile.cmake
@@ -0,0 +1,2 @@
+set(VCPKG_POLICY_EMPTY_PACKAGE enabled)
+message(STATUS "The port will install 'nvidia-cudnn-frontend' instead")
--- ports/nvidia-cudnn-frontend/portfile.cmake
+++ ports/nvidia-cudnn-frontend/portfile.cmake
@@ -2,14 +2,17 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO NVIDIA/cudnn-frontend
     REF v${VERSION}
-    SHA512 012aae3d81a92a19b4fd3067c1bf253dbfb820e852edba0150625aa733244d57001eeedf3c0e7382fb3b42dd10f385075c279470208d49199b02d5df45f712b1
+    SHA512 2d50fbedc1d2f488275aedce84893447a025d4c00b9e8609c4004b2eb0525480a348835d0e8b2784499d80d0c63d75bb1430741cb06c3652da8dd72b822489fa
     HEAD_REF main
+    PATCHES
+        fix-install.patch # support find_package(cudnn-frontend)
+        fix-thirdparty.patch
 )
+file(REMOVE_RECURSE "${SOURCE_PATH}/include/cudnn_frontend/thirdparty")
+file(COPY "${CMAKE_CURRENT_LIST_DIR}/config-template.cmake" DESTINATION "${SOURCE_PATH}/cmake")
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
     FEATURES
-        test    CUDNN_FRONTEND_BUILD_UNIT_TESTS
-        samples CUDNN_FRONTEND_BUILD_SAMPLES
         python  CUDNN_FRONTEND_BUILD_PYTHON_BINDINGS
 )
 
@@ -17,12 +20,18 @@ vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         ${FEATURE_OPTIONS}
+        -DCUDNN_FRONTEND_FETCH_PYBINDS_IN_CMAKE=OFF
+        -DCUDNN_FRONTEND_BUILD_TESTS=OFF
+        -DCUDNN_FRONTEND_BUILD_SAMPLES=OFF
         -DCUDNN_FRONTEND_SKIP_JSON_LIB=OFF
+    MAYBE_UNUSED_VARIABLES
+        CUDNN_FRONTEND_FETCH_PYBINDS_IN_CMAKE
 )
 vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake PACKAGE_NAME cudnn-frontend)
 
 file(REMOVE_RECURSE
     "${CURRENT_PACKAGES_DIR}/debug"
-    "${CURRENT_PACKAGES_DIR}/share"
+    "${CURRENT_PACKAGES_DIR}/lib"
 )
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE.txt")
