22d1407a44ede8d40c30c76d65a679e8b47729bc
luncliff <luncliff@gmail.com>
2025-03-31 23:44:57 +0900

    [opencl] update to v2024.10.24
    * https://github.com/KhronosGroup/OpenCL-ICD-Loader/releases/tag/v2024.10.24

--- ports/opencl/portfile.cmake
+++ ports/opencl/portfile.cmake
@@ -3,8 +3,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO KhronosGroup/OpenCL-ICD-Loader
-    REF v2024.05.08
-    SHA512 12d62183e49c5a1f813807291744d816008afca55b09f5acf2eef1bce50a453bf35a8dfbeb5f433022b0c5517f0a210d7123a3bac7a15ea63cc10f3bc71510f0
+    REF ${VERSION}
+    SHA512 29043eff21076440046314edf62bb488b7e4e17d9fbdac4c3727d8e2523c0c8fbf89ee7fcf762528af761ddbcb4be24e5f062ffa82f778401d6365faa35344a8
     HEAD_REF main
 )
 
@@ -12,15 +12,13 @@ vcpkg_cmake_configure(
   SOURCE_PATH "${SOURCE_PATH}"
   OPTIONS
     -DENABLE_OPENCL_LAYERINFO=OFF
-    -DOPENCL_ICD_LOADER_HEADERS_DIR:PATH=${CURRENT_INSTALLED_DIR}/include
+    "-DOPENCL_ICD_LOADER_HEADERS_DIR:PATH=${CURRENT_INSTALLED_DIR}/include"
 )
 
 vcpkg_cmake_build(TARGET OpenCL)
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(CONFIG_PATH share/cmake/OpenCLICDLoader PACKAGE_NAME OpenCLICDLoader)
-if(NOT VCPKG_TARGET_IS_WINDOWS)
-  vcpkg_fixup_pkgconfig()
-endif()
+vcpkg_fixup_pkgconfig()
 vcpkg_copy_pdbs()
 
 file(REMOVE_RECURSE
