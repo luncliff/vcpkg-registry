29d94adce39279aad67b6a6fb0dc5082c2e2fd3f
luncliff <luncliff@gmail.com>
2025-04-01 00:03:05 +0900

    [nvidia-cnmem] support CMake 4.0 build

--- ports/nvidia-cnmem/portfile.cmake
+++ ports/nvidia-cnmem/portfile.cmake
@@ -6,6 +6,8 @@ vcpkg_from_github(
     REF 37896cc9bfc6536a8c878a1e675835c22d827821
     SHA512 f6266c94041a546add582c2ad2a93dc7bfdd4c37fdd62df720b8c212249d1513ee89d975f3d0b1e555a911441abdc79c117ff6d8ca82563eaf6cbcc9140b8584
     HEAD_REF master
+    PATCHES
+        fix-cmake.patch
 )
 
 vcpkg_cmake_configure(
