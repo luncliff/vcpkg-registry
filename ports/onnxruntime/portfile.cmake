0595f5b932d08edf4735d915d7b6d8f137448b73
luncliff <luncliff@gmail.com>
2025-04-06 01:36:41 +0900

    [onnxruntime] remove CoreML support
    * requires many internal headers
    * training, framework can't be combined in Apple platform

--- ports/onnxruntime/portfile.cmake
+++ ports/onnxruntime/portfile.cmake
@@ -10,10 +10,10 @@ vcpkg_from_github(
     SHA512 028a7f48f41d2e8a453aae25ebc4cd769db389401937928b7d452fab5f8d7af8cb63eb4150daf79589845528f0e4c3bdfefa27af70d3630398990c9e8b85387b
     PATCHES
         fix-sources.patch
+        fix-cmake.patch
         fix-cmake-cuda.patch
         fix-cmake-training.patch
         fix-cmake-tensorrt.patch
-        # fix-cmake-coreml.patch
 )
 
 find_program(PROTOC NAMES protoc PATHS "${CURRENT_HOST_INSTALLED_DIR}/tools/protobuf" REQUIRED NO_DEFAULT_PATH NO_CMAKE_PATH)
@@ -86,12 +86,6 @@ if("tensorrt" IN_LIST FEATURES)
         message(WARNING "Define TENSORRT_HOME in the triplet for onnxruntime_TENSORRT_HOME")
     endif()
 endif()
-if("coreml" IN_LIST FEATURES)
-    list(APPEND FEATURE_OPTIONS
-        -D_enable_ML_PROGRAM=OFF # do not build CoreML Tools program
-        "-DCOREML_PROTO_ROOT:PATH=${CURRENT_INSTALLED_DIR}/include/mlmodel/format"
-    )
-endif()
 
 # see vcpkg_check_linkage above ...
 string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" BUILD_SHARED)
@@ -146,9 +140,6 @@ if("tensorrt" IN_LIST FEATURES)
 endif()
 if("directml" IN_LIST FEATURES)
     vcpkg_cmake_build(TARGET onnxruntime_providers_dml LOGFILE_BASE build-directml)
-endif()
-if("coreml" IN_LIST FEATURES)
-    vcpkg_cmake_build(TARGET onnxruntime_providers_coreml LOGFILE_BASE build-coreml)
 endif()
 if("training" IN_LIST FEATURES)
     vcpkg_cmake_build(TARGET tensorboard LOGFILE_BASE build-tensorboard)
