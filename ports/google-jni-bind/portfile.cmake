99ac29df3f707e6bbc2eab9b7d1a878dec94b587
PARK DongHa <luncliff@gmail.com>
2025-04-18 06:52:25 +0900

    [google-jni-bind] Update to 1.2.1-beta (#363)
    * [google-jni-bind] update to 1.2.1-beta
    
    * https://github.com/google/jni-bind/releases/tag/Release-1.2.1-beta
    
    * [google-jni-bind] update baseline

--- ports/google-jni-bind/portfile.cmake
+++ ports/google-jni-bind/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO google/jni-bind
-    REF Release-1.2.0
-    SHA512 ef1274b649d3cc46346a71cdf1269f5022983633feea2ec85200ea5dc7cc7fab8cac987aa3ca7514c1b8a3dcd7deea6f152b432d9d16ab230bf20bb16ff14a56
+    REF "Release-${VERSION}"
+    SHA512 1c5ad5c2731a54164debe973cbcc5265185dfbe7a9217193a7869268c1384b316dc79c95918e9acc27c872de63879d686a8378ae7a7bd394795d9ce861c63a1d
     HEAD_REF main
 )
 
