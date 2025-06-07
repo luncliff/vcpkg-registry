0bb9e84ac958e0c66ad961ce32de2f6b4003ee7d
luncliff <luncliff@gmail.com>
2025-06-07 21:52:30 +0900

    [skip ci] trying gn build of v8 project

--- ports/v8/portfile.cmake
+++ ports/v8/portfile.cmake
@@ -1,18 +1,4 @@
-
-vcpkg_from_github(
-    OUT_SOURCE_PATH SOURCE_PATH
-    REPO v8/v8
-    REF 13.9.90
-    SHA512 0
-)
-
-# vcpkg_get_windows_sdk(WINDOWS_SDK)
-# if (WINDOWS_SDK MATCHES "10.")
-#     set(LIBFILEPATH "$ENV{WindowsSdkDir}Lib\\${WINDOWS_SDK}\\um\\${TRIPLET_SYSTEM_ARCH}\\Ws2_32.Lib")
-#     set(HEADERSPATH "$ENV{WindowsSdkDir}Include\\${WINDOWS_SDK}\\um")
-# else()
-#     message(FATAL_ERROR "Portfile not yet configured for Windows SDK with version: ${WINDOWS_SDK}")
-# endif()
+# https://v8.dev/docs/build
 
 vcpkg_find_acquire_program(GIT)
 get_filename_component(GIT_PATH "${GIT}" DIRECTORY)
@@ -29,6 +15,51 @@ vcpkg_add_to_path(PREPEND "${NINJA_PATH}")
 vcpkg_find_acquire_program(PKGCONFIG)
 get_filename_component(PKGCONFIG_PATH "${PKGCONFIG}" DIRECTORY)
 vcpkg_add_to_path(PREPEND "${PKGCONFIG_PATH}")
+
+x_vcpkg_get_python_packages(
+    PYTHON_VERSION 3
+    OUT_PYTHON_VAR PYTHON3
+    PACKAGES httplib2
+)
+message(STATUS "Using python: ${PYTHON3}")
+get_filename_component(PYTHON_PATH "${PYTHON3}" DIRECTORY)
+vcpkg_add_to_path(PREPEND "${PYTHON_PATH}")
+
+vcpkg_from_git(
+    OUT_SOURCE_PATH TOOLS_SOURCE_PATH
+    URL "https://chromium.googlesource.com/chromium/tools/depot_tools.git"
+    REF 6d52c22ee313eb5701f326a9eca98acb41b669b4
+)
+vcpkg_add_to_path(PREPEND "${TOOLS_SOURCE_PATH}")
+
+find_program(GCLIENT NAMES gclient.py PATHS "${TOOLS_SOURCE_PATH}" REQUIRED)
+message(STATUS "Using gclient: ${GCLIENT}")
+
+vcpkg_download_distfile(ARCHIVE
+    URLS "https://github.com/v8/v8/archive/refs/tags/${VERSION}.zip"
+    FILENAME "v8-${VERSION}.zip"
+    SHA512 3d8ce9f2071cbba26788d3d570a92ca609700bdf4094563e9c654d3e26a10dfee150b1412870f6a051cdb6511b122de3aa3d98d9a7b751a83cd8b5d9f3f6c950
+)
+
+vcpkg_extract_source_archive(SOURCE_PATH
+    ARCHIVE "${ARCHIVE}"
+    # PATCHES
+    #     # ...
+)
+
+vcpkg_execute_required_process(
+    COMMAND "${PYTHON3}" "${GCLIENT}" sync
+    WORKING_DIRECTORY ${SOURCE_PATH}
+    LOGNAME gclient-sync
+)
+
+# vcpkg_get_windows_sdk(WINDOWS_SDK)
+# if (WINDOWS_SDK MATCHES "10.")
+#     set(LIBFILEPATH "$ENV{WindowsSdkDir}Lib\\${WINDOWS_SDK}\\um\\${TRIPLET_SYSTEM_ARCH}\\Ws2_32.Lib")
+#     set(HEADERSPATH "$ENV{WindowsSdkDir}Include\\${WINDOWS_SDK}\\um")
+# else()
+#     message(FATAL_ERROR "Portfile not yet configured for Windows SDK with version: ${WINDOWS_SDK}")
+# endif()
 
 vcpkg_add_to_path(PREPEND "${CURRENT_INSTALLED_DIR}/bin")
 vcpkg_add_to_path(PREPEND "${CURRENT_INSTALLED_DIR}/debug/bin")
