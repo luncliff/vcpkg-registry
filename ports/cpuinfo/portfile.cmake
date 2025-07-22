3f537226696a27e61b3a54dd6123b938512241c9
PARK DongHa <luncliff@gmail.com>
2025-07-22 11:06:19 +0900

    [cpuinfo] update to 2025-06-26 (#404)
    * [cpuinfo] update to 2025-06-26
    
    * [cpuinfo] update baseline

--- ports/cpuinfo/portfile.cmake
+++ ports/cpuinfo/portfile.cmake
@@ -6,8 +6,8 @@ endif()
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO pytorch/cpuinfo
-    REF 39ea79a3c132f4e678695c579ea9353d2bd29968
-    SHA512 7328bc6991d386aaee4f94353749216a2413880f36ef1912cc966c7b5720054cc7df8cc1fe26dd87b2f4aff2b78b17f18ecae9d84a0b10453dc9866117542814
+    REF e4cadd02a8b386c38b84f0a19eddacec3f433baa
+    SHA512 aaf239e4a322f04514c8d85d4792abb6b4f5058be9f2013e42e51ecff66e3c967339fa3e030373aed8aebefaf8b0a287cf941de854ae7ade99fe6af4213f78e9
     HEAD_REF main
 )
 
@@ -16,31 +16,48 @@ vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
         tools CPUINFO_BUILD_TOOLS
 )
 
+set(LINK_OPTIONS "")
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
+    list(APPEND LINK_OPTIONS -DCPUINFO_LIBRARY_TYPE=shared)
+else()
+    list(APPEND LINK_OPTIONS -DCPUINFO_LIBRARY_TYPE=static)
+endif()
+
+if(VCPKG_CRT_LINKAGE STREQUAL "dynamic")
+    list(APPEND LINK_OPTIONS -DCPUINFO_RUNTIME_TYPE=shared)
+else()
+    list(APPEND LINK_OPTIONS -DCPUINFO_RUNTIME_TYPE=static)
+endif()
+
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         ${FEATURE_OPTIONS}
+        ${LINK_OPTIONS}
         -DCPUINFO_BUILD_UNIT_TESTS=OFF
         -DCPUINFO_BUILD_MOCK_TESTS=OFF
         -DCPUINFO_BUILD_BENCHMARKS=OFF
-        -DCPUINFO_BUILD_PKG_CONFIG=ON
     OPTIONS_DEBUG
         -DCPUINFO_LOG_LEVEL=debug
     OPTIONS_RELEASE
         -DCPUINFO_LOG_LEVEL=default
 )
 vcpkg_cmake_install()
-vcpkg_cmake_config_fixup(CONFIG_PATH share/cpuinfo PACKAGE_NAME ${PORT})
+vcpkg_cmake_config_fixup()
+vcpkg_copy_pdbs()
 vcpkg_fixup_pkgconfig() # pkg_check_modules(libcpuinfo)
 
-if("tools" IN_LIST FEATURES)
-    find_program(CPUID_DUMP NAMES cpuid-dump PATHS "${CURRENT_PACKAGES_DIR}/bin")
-    if(CPUID_DUMP)
-        vcpkg_copy_tools(TOOL_NAMES cpuid-dump AUTO_CLEAN)
-    endif()
-    vcpkg_copy_tools(TOOL_NAMES cache-info cpu-info isa-info AUTO_CLEAN)
-endif()
-
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+
+if("tools" IN_LIST FEATURES)
+    set(additional_tools "")
+    if(EXISTS "${CURRENT_PACKAGES_DIR}/bin/cpuid-dump${VCPKG_TARGET_EXECUTABLE_SUFFIX}")
+        list(APPEND additional_tools "cpuid-dump")
+    endif()
+    vcpkg_copy_tools(
+        TOOL_NAMES cache-info cpu-info isa-info ${additional_tools}
+        AUTO_CLEAN
+    )
+endif()
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
