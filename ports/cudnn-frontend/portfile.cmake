ee588fd62c5344342475de8dd89458b28e7809df
luncliff <luncliff@gmail.com>
2025-08-05 01:19:52 +0900

    [cudnn-frontend] update to 1.13.0
    - https://github.com/NVIDIA/cudnn-frontend/releases/tag/v1.13.0

--- ports/cudnn-frontend/portfile.cmake
+++ ports/cudnn-frontend/portfile.cmake
@@ -1,2 +1,38 @@
-set(VCPKG_POLICY_EMPTY_PACKAGE enabled)
-message(STATUS "The port will install 'nvidia-cudnn-frontend' instead")
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO NVIDIA/cudnn-frontend
+    REF v${VERSION}
+    SHA512 0be8fbe1303be43fef2b8b0cd38c45e25cce4f911762d303bf00e7c5161faebcee583cda6608618433fe1637e52a4b5f5237e38c52dde4344ca12d6a7d2fe5b0
+    HEAD_REF main
+    PATCHES
+        fix-thirdparty.patch
+)
+file(REMOVE_RECURSE "${SOURCE_PATH}/include/cudnn_frontend/thirdparty")
+
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    FEATURES
+        python  CUDNN_FRONTEND_BUILD_PYTHON_BINDINGS
+)
+
+# header only, INTERFACE library
+set(VCPKG_BUILD_TYPE release)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        ${FEATURE_OPTIONS}
+        -DCUDNN_FRONTEND_FETCH_PYBINDS_IN_CMAKE=OFF
+        -DCUDNN_FRONTEND_BUILD_TESTS=OFF
+        -DCUDNN_FRONTEND_BUILD_SAMPLES=OFF
+        -DCUDNN_FRONTEND_SKIP_JSON_LIB=OFF
+    MAYBE_UNUSED_VARIABLES
+        CUDNN_FRONTEND_FETCH_PYBINDS_IN_CMAKE
+)
+vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/cudnn_frontend PACKAGE_NAME cudnn_frontend)
+
+file(REMOVE_RECURSE
+    "${CURRENT_PACKAGES_DIR}/debug"
+    "${CURRENT_PACKAGES_DIR}/lib"
+)
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE.txt")
