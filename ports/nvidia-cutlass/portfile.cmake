8973e0b17bc44c57dd2171e44e5f43b36199791a
PARK DongHa <luncliff@gmail.com>
2025-08-06 00:23:20 +0900

    [nvidia-cutlass] update to 3.9.0  (#412)
    - https://github.com/NVIDIA/cutlass/releases/tag/v3.9.0
    * [nvidia-cutlass] use `vcpkg_find_cuda`, use tools by default
    * ci: test nvidia-cutlass[tools]
    * [nvidia-cutlass] 'library' feature

--- ports/nvidia-cutlass/portfile.cmake
+++ ports/nvidia-cutlass/portfile.cmake
@@ -1,18 +1,24 @@
 if(VCPKG_TARGET_IS_WINDOWS)
     vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+    vcpkg_buildpath_length_warning(50)
 endif()
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO NVIDIA/cutlass
     REF v${VERSION}
-    SHA512 a08aac281fb3bdea82c0a044dc643c40e4803d02e55bbea450021cb7a5472aed86e79c5df41cd981976af8403f18cc48d8069045c4e68339430d3a3caeb109ac
+    SHA512 b02775b7a364a708388fd7ce1c5526453cfb42f35fd9143aad0b0efd2dc10b66feb1539ad08f0f9089fd241e0bdb2002fdeea2604cfdba7a519aceb4f0df0557
     HEAD_REF main
 )
 
 vcpkg_find_acquire_program(PYTHON3)
 get_filename_component(PYTHON_PATH "${PYTHON3}" PATH)
 vcpkg_add_to_path(PREPEND "${PYTHON_PATH}")
+
+vcpkg_find_cuda(OUT_CUDA_TOOLKIT_ROOT cuda_toolkit_root)
+
+# In some CMake version, the enable_language(CUDA) may report architecture error
+set(CUDA_ARCHS "native")
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
     FEATURES
@@ -22,24 +28,33 @@ vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
         tests   CUTLASS_TEST_UNIT_ENABLE_WARNINGS
         samples CUTLASS_ENABLE_EXAMPLES
         tools   CUTLASS_ENABLE_TOOLS
-        tools   CUTLASS_ENABLE_LIBRARY
+        library CUTLASS_ENABLE_LIBRARY # too many .cu files
 )
+
+# Simple header-only library. With "library" feature, just release artifacts
+set(VCPKG_BUILD_TYPE release)
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         ${FEATURE_OPTIONS}
+        "-DCMAKE_CUDA_ARCHITECTURES=${CUDA_ARCHS}"
+        "-DCMAKE_CUDA_COMPILER=${NVCC}"
+        "-DCMAKE_CUDA_FLAGS=-Xcudafe --diag_suppress=20012"
+        "-DCUDAToolkit_ROOT=${cuda_toolkit_root}"
         "-DCUTLASS_REVISION:STRING=v${VERSION}"
-        -DCUTLASS_ENABLE_HEADERS_ONLY=ON
-        -DCUTLASS_ENABLE_PERFORMANCE=OFF
+        "-DPython3_EXECUTABLE:FILEPATH=${PYTHON3}"
         -DCUTLASS_LIBRARY_OPERATIONS:STRING=all
         -DCUTLASS_LIBRARY_KERNELS:STRING=all
+        -DCUTLASS_ENABLE_HEADERS_ONLY=ON
+        -DCUTLASS_ENABLE_PERFORMANCE=OFF
         -DCUTLASS_ENABLE_CUBLAS=ON
         -DCUTLASS_ENABLE_CUDNN=ON
         -DCUTLASS_ENABLE_PROFILER=OFF
-        "-DPython3_EXECUTABLE:FILEPATH=${PYTHON3}"
     MAYBE_UNUSED_VARIABLES
         CUTLASS_LIBRARY_OPERATIONS
+        CMAKE_CUDA_COMPILER
+        CUDAToolkit_ROOT
 )
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/NvidiaCutlass" PACKAGE_NAME "NvidiaCutlass")
@@ -47,6 +62,9 @@ vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/NvidiaCutlass" PACKAGE_NAME "Nvi
 file(REMOVE_RECURSE
     "${CURRENT_PACKAGES_DIR}/debug"
     "${CURRENT_PACKAGES_DIR}/test"
-    "${CURRENT_PACKAGES_DIR}/lib"
 )
+if(NOT("library" IN_LIST FEATURES))
+    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib")
+endif()
+
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE.txt")
