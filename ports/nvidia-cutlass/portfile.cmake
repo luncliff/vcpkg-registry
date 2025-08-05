756d96cc97bc33598c065f141c996e630b93e6c8
luncliff <luncliff@gmail.com>
2025-08-05 23:53:31 +0900

    [nvidia-cutlass] 'library' feature

--- ports/nvidia-cutlass/portfile.cmake
+++ ports/nvidia-cutlass/portfile.cmake
@@ -1,5 +1,6 @@
 if(VCPKG_TARGET_IS_WINDOWS)
     vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+    vcpkg_buildpath_length_warning(50)
 endif()
 
 vcpkg_from_github(
@@ -27,7 +28,11 @@ vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
         tests   CUTLASS_TEST_UNIT_ENABLE_WARNINGS
         samples CUTLASS_ENABLE_EXAMPLES
         tools   CUTLASS_ENABLE_TOOLS
+        library CUTLASS_ENABLE_LIBRARY # too many .cu files
 )
+
+# Simple header-only library. With "library" feature, just release artifacts
+set(VCPKG_BUILD_TYPE release)
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
@@ -35,13 +40,13 @@ vcpkg_cmake_configure(
         ${FEATURE_OPTIONS}
         "-DCMAKE_CUDA_ARCHITECTURES=${CUDA_ARCHS}"
         "-DCMAKE_CUDA_COMPILER=${NVCC}"
+        "-DCMAKE_CUDA_FLAGS=-Xcudafe --diag_suppress=20012"
         "-DCUDAToolkit_ROOT=${cuda_toolkit_root}"
         "-DCUTLASS_REVISION:STRING=v${VERSION}"
         "-DPython3_EXECUTABLE:FILEPATH=${PYTHON3}"
         -DCUTLASS_LIBRARY_OPERATIONS:STRING=all
         -DCUTLASS_LIBRARY_KERNELS:STRING=all
         -DCUTLASS_ENABLE_HEADERS_ONLY=ON
-        -DCUTLASS_ENABLE_LIBRARY=OFF # too many .cu files
         -DCUTLASS_ENABLE_PERFORMANCE=OFF
         -DCUTLASS_ENABLE_CUBLAS=ON
         -DCUTLASS_ENABLE_CUDNN=ON
@@ -57,6 +62,9 @@ vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/NvidiaCutlass" PACKAGE_NAME "Nvi
 file(REMOVE_RECURSE
     "${CURRENT_PACKAGES_DIR}/debug"
     "${CURRENT_PACKAGES_DIR}/test"
-    "${CURRENT_PACKAGES_DIR}/lib"
 )
+if(NOT("library" IN_LIST FEATURES))
+    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib")
+endif()
+
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE.txt")
