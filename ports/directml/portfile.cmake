4fdaacdfeb8e8a9ba9e5b1a08b09e1bc9fb73d6e
PARK DongHa <luncliff@gmail.com>
2025-10-04 15:54:51 +0900

    [directml] Update to 1.15.3 (#456)
    * [directml] update to version 1.15.3
    
    - https://www.nuget.org/packages/Microsoft.AI.DirectML/1.15.3
    
    * [directml] set vcpkg policy and build type
    
    * [directml] update baseline

--- ports/directml/portfile.cmake
+++ ports/directml/portfile.cmake
@@ -1,12 +1,16 @@
 vcpkg_check_linkage(ONLY_DYNAMIC_LIBRARY ONLY_DYNAMIC_CRT)
 vcpkg_find_acquire_program(NUGET)
 
+# DirectML provides DirectML.Debug.dll. They will be installed with release DLLs
+set(VCPKG_BUILD_TYPE release)
+set(VCPKG_POLICY_MISMATCHED_NUMBER_OF_BINARIES enabled)
+
 set(ENV{NUGET_PACKAGES} "${BUILDTREES_DIR}/nuget")
 
 # see https://www.nuget.org/packages/Microsoft.AI.DirectML/
 # see https://github.com/microsoft/DirectML/blob/master/Releases.md
 set(PACKAGE_NAME    "Microsoft.AI.DirectML")
-set(PACKAGE_VERSION "1.15.2")
+set(PACKAGE_VERSION "1.15.3")
 
 file(REMOVE_RECURSE "${CURRENT_BUILDTREES_DIR}/${PACKAGE_NAME}")
 vcpkg_execute_required_process(
@@ -46,11 +50,20 @@ else()
 endif()
 
 if(VCPKG_TARGET_IS_WINDOWS)
-    file(INSTALL "${SOURCE_PATH}/bin/${TRIPLE}/DirectML.lib"        DESTINATION "${CURRENT_PACKAGES_DIR}/lib")
-    file(INSTALL "${SOURCE_PATH}/bin/${TRIPLE}/DirectML.dll"        DESTINATION "${CURRENT_PACKAGES_DIR}/bin")
-    file(INSTALL "${SOURCE_PATH}/bin/${TRIPLE}/DirectML.pdb"        DESTINATION "${CURRENT_PACKAGES_DIR}/bin")
-    file(INSTALL "${SOURCE_PATH}/bin/${TRIPLE}/DirectML.Debug.dll"  DESTINATION "${CURRENT_PACKAGES_DIR}/bin")
-    file(INSTALL "${SOURCE_PATH}/bin/${TRIPLE}/DirectML.Debug.pdb"  DESTINATION "${CURRENT_PACKAGES_DIR}/bin")
+    file(INSTALL "${SOURCE_PATH}/bin/${TRIPLE}/DirectML.lib"
+        DESTINATION "${CURRENT_PACKAGES_DIR}/lib"
+    )
+    file(INSTALL "${SOURCE_PATH}/bin/${TRIPLE}/DirectML.dll"
+                 "${SOURCE_PATH}/bin/${TRIPLE}/DirectML.pdb"
+        DESTINATION "${CURRENT_PACKAGES_DIR}/bin"
+    )
+    # Install debug artifacts only if they exist upstream to avoid mismatched debug/release
+    if(EXISTS "${SOURCE_PATH}/bin/${TRIPLE}/DirectML.Debug.dll")
+        file(INSTALL "${SOURCE_PATH}/bin/${TRIPLE}/DirectML.Debug.dll"  DESTINATION "${CURRENT_PACKAGES_DIR}/bin")
+    endif()
+    if(EXISTS "${SOURCE_PATH}/bin/${TRIPLE}/DirectML.Debug.pdb")
+        file(INSTALL "${SOURCE_PATH}/bin/${TRIPLE}/DirectML.Debug.pdb"  DESTINATION "${CURRENT_PACKAGES_DIR}/bin")
+    endif()
 elseif(VCPKG_TARGET_IS_LINUX)
     file(INSTALL "${SOURCE_PATH}/bin/${TRIPLE}/libdirectml.so"  DESTINATION "${CURRENT_PACKAGES_DIR}/lib")
 else()
